2f38f1f11abf65339ea78634ff05d0b502bea2c7
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-05-26 17:21:58 +0200

    Added python parser to buildin python scripts

---
+++ flexiprep/src/main/java/nl/lumc/sasc/biopet/pipelines/flexiprep/scripts/FastqSync.scala
@@ -0,0 +1,36 @@
+package nl.lumc.sasc.biopet.pipelines.flexiprep.scripts
+
+//import java.io.FileOutputStream
+import nl.lumc.sasc.biopet.core._
+import nl.lumc.sasc.biopet.wrappers._
+import org.broadinstitute.sting.queue.function.CommandLineFunction
+import org.broadinstitute.sting.commandline._
+import java.io.File
+
+class FastqSync(val globalConfig: Config) extends CommandLineFunction with Python {
+  def this() = this(new Config(Map()))
+  analysisName = "fastqsync"
+  val config: Config = globalConfig.getAsConfig(analysisName)
+  logger.debug("Config for " + analysisName + ": " + config)
+  
+  setPythonScript("__init__.py", "scripts/pyfastqc/")
+  setPythonScript("sync_paired_end_reads.py", "scripts/")
+  
+  @Input(doc="Start fastq") var input_start_fastq: File = _
+  @Input(doc="R1 input") var input_R1: File = _
+  @Input(doc="R2 input") var input_R2: File = _
+  @Output(doc="R1 output") var output_R1: File = _
+  @Output(doc="R2 output") var output_R2: File = _
+  @Output(doc="stats output") var output_stats: File = _
+  
+  def commandLine = {
+    getPythonCommand + 
+    required(input_start_fastq) +
+    required(input_R1) +
+    required(input_R2) +
+    required(output_R1) +
+    required(output_R2) +
+    " > " +
+    required(output_stats)
+  }
+}
