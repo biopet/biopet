44703a4699d7bfee0bf2fbb41f2e92b0fb9becbf
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-06-03 11:51:12 +0200

    No functional changes

--- biopet-framework/src/main/java/nl/lumc/sasc/biopet/wrappers/Zcat.scala
+++ biopet-framework/src/main/java/nl/lumc/sasc/biopet/wrappers/Zcat.scala
@@ -6,13 +6,13 @@ import org.broadinstitute.sting.commandline._
 import java.io.File
 
 class Zcat(val globalConfig: Config) extends CommandLineFunction {
-    def this() = this(new Config(Map()))
-    this.analysisName = "zcat"
-    val config: Config = globalConfig.getAsConfig("zcat")
-    logger.debug("Config for " + this.analysisName + ": " + config)
-	  
-    @Input(doc="Zipped file") var in: File = _
-    @Output(doc="Unzipped file") var out: File = _
-    
-    def commandLine = "zcat %s > %s".format(in, out)
+  def this() = this(new Config(Map()))
+  this.analysisName = "zcat"
+  val config: Config = globalConfig.getAsConfig("zcat")
+  logger.debug("Config for " + this.analysisName + ": " + config)
+  
+  @Input(doc="Zipped file") var in: File = _
+  @Output(doc="Unzipped file") var out: File = _
+  
+  def commandLine = "zcat %s > %s".format(in, out)
 }
