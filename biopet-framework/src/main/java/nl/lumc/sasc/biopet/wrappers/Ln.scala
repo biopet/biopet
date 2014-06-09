4375a55d723882998be5630da6c384919ddb54ad
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-06-09 09:31:18 +0200

    Switched to InProcessFunction, no cluster job needed for ln commands

--- biopet-framework/src/main/java/nl/lumc/sasc/biopet/wrappers/Ln.scala
+++ biopet-framework/src/main/java/nl/lumc/sasc/biopet/wrappers/Ln.scala
@@ -1,28 +1,22 @@
 package nl.lumc.sasc.biopet.wrappers
 
 import nl.lumc.sasc.biopet.core._
-import org.broadinstitute.sting.queue.function.CommandLineFunction
-//import org.broadinstitute.sting.queue.function.QFunction
+import org.broadinstitute.sting.queue.function.InProcessFunction
 import org.broadinstitute.sting.commandline._
 import java.io.File
 import scala.sys.process._
 
-class Ln(val globalConfig: Config) extends CommandLineFunction {
+class Ln(val globalConfig: Config) extends InProcessFunction {
   def this() = this(new Config(Map()))
   this.analysisName = "ln"
   val config: Config = Config.mergeConfigs(globalConfig.getAsConfig(analysisName), globalConfig)
-  logger.debug("Config for " + this.analysisName + ": " + config)
   
   @Input(doc="Input file") var in: File = _
   @Output(doc="Link destination") var out: File = _
     
-  def commandLine = {
-    "ln -sf " + required(in) + required(out)
-  }
-}
-
-object Ln {
-  def addLn(input:File, output:File) : Ln = {
-    return new Ln { this.in = input; this.out = output }
+  override def run {
+    val cmd = "ln -s " + in + " " + out
+    val exitcode = cmd.!
+    System.out.println("cmd: '" + cmd + "', exitcode: " + exitcode)
   }
 }
