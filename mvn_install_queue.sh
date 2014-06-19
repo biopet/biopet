98a8dbbd527a10df6eabb12931339227c4df3d1a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-06-19 10:57:05 +0200

    Readme changed to README.md

--- mvn_install_queue.sh
+++ mvn_install_queue.sh
@@ -1,2 +1,22 @@
-QUEUE_JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/Queue-shark.jar
-mvn install:install-file -Dfile=$QUEUE_JAR -DgroupId=org.broadinstitute.sting -DartifactId=queue-package -Dversion=3.1 -Dpackaging=jar
+JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/Queue-shark.jar
+ID=queue-package
+VERSION=3.1
+GROUP=org.broadinstitute.sting
+mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
+
+JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/queue-framework-3.1.jar
+ID=queue-framework
+mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
+
+#gatk-package
+
+JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/gatk-package-3.1.jar
+ID=gatk-package
+mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
+
+#gatk-framework
+
+JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/gatk-framework-3.1.jar
+ID=gatk-framework
+mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
+
