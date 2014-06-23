d60a291ad4c3e2d31b7930667942ffdc9b0ef174
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-06-23 12:42:22 +0200

    General QScript with default configfiles, outputDir and outputFiles

--- biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/BiopetQScript.scala
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/BiopetQScript.scala
@@ -3,54 +3,23 @@ package nl.lumc.sasc.biopet.core
 //import org.broadinstitute.sting.queue.QScript
 import java.io.File
 import nl.lumc.sasc.biopet.core.config._
+import org.broadinstitute.sting.commandline._
 
-abstract trait BiopetQScript extends Configurable {
-  //var config: Config = _
+trait BiopetQScript extends Configurable {
+  @Argument(doc="Config Json file",shortName="config", required=false)
+  val configfiles: List[File] = Nil
+  
+  @Argument(doc="Output directory", shortName="outputDir", required=true)
+  var outputDir: String = _
+  
   var outputFiles:Map[String,File] = Map()
-  var samples:Map[String,Any] = Map()
   
-  final def runSamplesJobs : Map[String,Map[String,File]] = {
-    var output: Map[String,Map[String,File]] = Map()
-    samples = config("samples")
-    if (globalConfig.contains("samples")) for ((key,value) <- samples) {
-      var sample = Configurable.any2map(value)
-      if (!sample.contains("ID")) sample += ("ID" -> key)
-      if (sample("ID") == key) {
-        var files:Map[String,List[File]] = runSingleSampleJobs(sample)
-      } else logger.warn("Key is not the same as ID on value for sample")
-    } else logger.warn("No Samples found in config")
-    return output
-  }
+  def init
+  def biopetScript
   
-  def runSingleSampleJobs(sample:String) : Map[String,List[File]] ={
-    return runSingleSampleJobs(Configurable.any2map(samples(sample)))
-  }
-  def runSingleSampleJobs(sampleConfig:Map[String,Any]) : Map[String,List[File]] = {
-    logger.debug("Default sample function, function 'runSingleSampleJobs' not defined in pipeline")
-    
-    runRunsJobs(sampleConfig)
-    
-    return Map()
-  }
-  
-  final def runRunsJobs(sampleConfig:Map[String,Any]) : Map[String,Map[String,File]] = {
-    var output: Map[String,Map[String,File]] = Map()
-    val sampleID = sampleConfig("ID")
-    if (sampleConfig.contains("runs")) {
-      val runs = Configurable.any2map(sampleConfig("runs"))
-      for ((key,value) <- runs) {
-        var run = Configurable.any2map(value)
-        if (!run.contains("ID")) run += ("ID" -> key)
-        if (run("ID") == key) {
-          output += key -> runSingleRunJobs(run, sampleConfig)
-        } else logger.warn("Key is not the same as ID on value for run of sample: " + sampleID)
-      }
-    } else logger.warn("No runs found in config for sample: " + sampleID)
-    return output
-  }
-  def runSingleRunJobs(runConfig:Map[String,Any], sampleConfig:Map[String,Any]) : Map[String,File] = {
-    logger.debug("Default run function, function 'runSingleRunJobs' not defined in pipeline")
-    
-    return Map()
+  final def script() {
+    init
+    biopetScript
+    // TODO: Config report
   }
 }
