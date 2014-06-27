cafd9cd79f25ae420b5803545164d0e35c01c070
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-06-27 12:57:33 +0200

    Remove unused import

--- biopet-framework/src/main/scala/nl/lumc/sasc/biopet/function/Zcat.scala
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/function/Zcat.scala
@@ -2,7 +2,6 @@ package nl.lumc.sasc.biopet.function
 
 import nl.lumc.sasc.biopet.core._
 import nl.lumc.sasc.biopet.core.config._
-//import org.broadinstitute.sting.queue.function.CommandLineFunction
 import org.broadinstitute.sting.commandline._
 import java.io.File
 
