97327078dcec1a39775995607c2989d4c50f4c08
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-07-03 13:58:18 +0200

    First version of working bam-metrics pipeline

--- bam-metrics/src/main/scala/nl/lumc/sasc/biopet/pipelines/bammetrics/BamMetrics.scala
+++ bam-metrics/src/main/scala/nl/lumc/sasc/biopet/pipelines/bammetrics/BamMetrics.scala
@@ -1,27 +1,80 @@
 package nl.lumc.sasc.biopet.pipelines.bammetrics
 
-import nl.lumc.sasc.biopet.core._
-import nl.lumc.sasc.biopet.core.config._
-import nl.lumc.sasc.biopet.function._
+import nl.lumc.sasc.biopet.pipelines.bammetrics.scripts.CoverageStats
 import org.broadinstitute.sting.queue.QScript
-import org.broadinstitute.sting.queue.extensions.picard._
-import org.broadinstitute.sting.queue.function._
-import org.broadinstitute.sting.commandline._
+import nl.lumc.sasc.biopet.core.{BiopetQScript, PipelineCommand}
+import java.io.File
+import nl.lumc.sasc.biopet.core.apps.{BedToInterval, BiopetFlagstat}
+import nl.lumc.sasc.biopet.core.config.Configurable
+import nl.lumc.sasc.biopet.function.bedtools.{BedtoolsCoverage, BedtoolsIntersect}
+import nl.lumc.sasc.biopet.function.picard.{CollectInsertSizeMetrics, CollectGcBiasMetrics, CalculateHsMetrics}
+import nl.lumc.sasc.biopet.function.samtools.SamtoolsFlagstat
 
 class BamMetrics(val root:Configurable) extends QScript with BiopetQScript {
   def this() = this(null)
   
   @Input(doc="Bam File", shortName="BAM",required=true)
-  var input_bam: File = _
+  var inputBam: File = _
+    
+  @Input(doc="Bed tracks targets", shortName="target",required=false)
+  var bedFiles: List[File] = Nil
+  
+  @Input(doc="Bed tracks bait", shortName="bait",required=false)
+  var baitBedFile: File = _
+  
+  @Argument(doc="",required=false)
+  var wholeGenome = false
   
   def init() {
     for (file <- configfiles) globalConfig.loadConfigFile(file)
+    if (outputDir == null) throw new IllegalStateException("Missing Output directory on BamMetrics module")
+    else if (!outputDir.endsWith("/")) outputDir += "/"
   }
   
   def biopetScript() {
+    add(SamtoolsFlagstat(this, inputBam, outputDir))
+    add(BiopetFlagstat(this, inputBam, outputDir))
+    add(CollectGcBiasMetrics(this, inputBam, outputDir))
+    add(CollectInsertSizeMetrics(this, inputBam, outputDir))
+    
+    val baitIntervalFile = if (baitBedFile != null) new File(outputDir, baitBedFile.getName.stripSuffix(".bed") + ".interval") else null
+    if (baitIntervalFile != null)
+      add(BedToInterval(this, baitBedFile, inputBam, outputDir), true)
+    
+    for (bedFile <- bedFiles) {
+      val targetDir = outputDir + bedFile.getName.stripSuffix(".bed") + "/"
+      val targetInterval = BedToInterval(this, bedFile, inputBam, targetDir)
+      add(targetInterval, true)
+      add(CalculateHsMetrics(this, inputBam, if (baitIntervalFile != null) baitIntervalFile
+                             else targetInterval.output, targetInterval.output, targetDir))
+      
+      val strictOutputBam = new File(targetDir, inputBam.getName.stripSuffix(".bam") + ".overlap.strict.bam")
+      add(BedtoolsIntersect(this, inputBam, bedFile, strictOutputBam, minOverlap = config("strictintersectoverlap", 1.0)), true)
+      add(SamtoolsFlagstat(this, strictOutputBam))
+      add(BiopetFlagstat(this, strictOutputBam, targetDir))
+      
+      val looseOutputBam = new File(targetDir, inputBam.getName.stripSuffix(".bam") + ".overlap.loose.bam")
+      add(BedtoolsIntersect(this, inputBam, bedFile, looseOutputBam, minOverlap = config("looseintersectoverlap", 0.01)), true)
+      add(SamtoolsFlagstat(this, looseOutputBam))
+      add(BiopetFlagstat(this, looseOutputBam, targetDir))
+      
+      val coverageFile = new File(targetDir, inputBam.getName.stripSuffix(".bam") + ".coverage")
+      add(BedtoolsCoverage(this, inputBam, bedFile, coverageFile, true), true)
+      add(CoverageStats(this, coverageFile, targetDir))
+    }
   }
 }
 
 object BamMetrics extends PipelineCommand {
   override val pipeline = "/nl/lumc/sasc/biopet/pipelines/bammetrics/BamMetrics.class"
+  
+  def apply(root:Configurable, bamFile:File, outputDir:String) : BamMetrics = {
+    val bamMetrics = new BamMetrics(root)
+    bamMetrics.inputBam = bamFile
+    bamMetrics.outputDir = outputDir
+    
+    bamMetrics.init
+    bamMetrics.biopetScript
+    return bamMetrics
+  }
 }
---
+++ bam-metrics/src/main/scala/nl/lumc/sasc/biopet/pipelines/bammetrics/scripts/CoverageStats.scala
@@ -0,0 +1,33 @@
+package nl.lumc.sasc.biopet.pipelines.bammetrics.scripts
+
+import nl.lumc.sasc.biopet.core._
+import nl.lumc.sasc.biopet.core.config._
+import nl.lumc.sasc.biopet.function.PythonCommandLineFunction
+import org.broadinstitute.sting.commandline._
+import java.io.File
+
+class CoverageStats(val root:Configurable) extends PythonCommandLineFunction {
+  setPythonScript("bedtools_cov_stats.py")
+  
+  @Input(doc="Input file")
+  var input: File = _
+  
+  @Output(doc="output File")
+  var output: File = _
+  
+  @Output(doc="plot File (png)")
+  var plot: File = _
+  
+  def cmdLine = getPythonCommand +
+    required(input) + required("--plot", plot) + " > " + required(output)
+}
+
+object CoverageStats {
+  def apply(root:Configurable, input:File, outputDir:String) : CoverageStats = {
+    val coverageStats = new CoverageStats(root)
+    coverageStats.input = input
+    coverageStats.output = new File(outputDir, input.getName + ".stats")
+    coverageStats.plot = new File(outputDir, input.getName + ".stats.png")
+    return coverageStats
+  }
+}
