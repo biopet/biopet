94c23ca3b33f5e2aa51a989d8a0ac544d893e650
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-07-18 15:58:39 +0200

    Fix for Queue-3.2-2

--- mvn_install_queue.sh
+++ mvn_install_queue.sh
@@ -1,22 +1,6 @@
-JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/Queue-shark.jar
+JAR=/data/DIV5/SASC/common/programs/Queue-3.2-2/Queue-shark.jar
 ID=queue-package
-VERSION=3.1
-GROUP=org.broadinstitute.sting
-mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
-
-JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/queue-framework-3.1.jar
-ID=queue-framework
-mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
-
-#gatk-package
-
-JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/gatk-package-3.1.jar
-ID=gatk-package
-mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
-
-#gatk-framework
-
-JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/gatk-framework-3.1.jar
-ID=gatk-framework
+VERSION=3.2-2
+GROUP=org.broadinstitute.gatk
 mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
 
