79435a295a0a4994979b1d6e5c75966c1d2e474c
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-07-21 10:14:54 +0200

    Collected all gatk pipelines/artifacts into 1 maven project

--- mapping/run_mapping.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-JAVA_EXE=java
-QUEUE_JAR=/data/DIV5/SASC/common/programs/Queue-3.1-1/Queue.jar
-JAR_FILE=`dirname $0`/target/Mapping*.jar
-PIPELINE_SCRIPT=`dirname $0`/src/main/java/nl/lumc/sasc/biopet/pipelines/mapping/Mapping.scala
-
-$JAVA_EXE -Xmx5g $JAVA_OPTIONS -jar $JAR_FILE -S $PIPELINE_SCRIPT $@
