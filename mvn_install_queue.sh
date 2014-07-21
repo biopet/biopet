89926ccdf8ba90019bba085010548d91daf7d44c
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-07-21 11:22:10 +0200

    Fixed location of jar file

--- mvn_install_queue.sh
+++ mvn_install_queue.sh
@@ -1,6 +1,7 @@
-JAR=/data/DIV5/SASC/common/programs/Queue-3.2-2/Queue-shark.jar
-ID=queue-package
-VERSION=3.2-2
+JAR=/data/DIV5/SASC/common/programs/Queue-3.2-2/Queue.jar
+ID=gatk-queue-package-distribution
+VERSION=3.2
 GROUP=org.broadinstitute.gatk
+
 mvn install:install-file -Dfile=$JAR -DgroupId=$GROUP -DartifactId=$ID -Dversion=$VERSION -Dpackaging=jar
 
