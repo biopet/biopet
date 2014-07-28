8e67bf4ff1910c42b580f587cb5954d97e8b55b2
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-07-28 18:35:01 +0200

    Seqstat now uses quality type data

--- flexiprep/src/main/scala/nl/lumc/sasc/biopet/pipelines/flexiprep/scripts/Seqstat.scala
+++ flexiprep/src/main/scala/nl/lumc/sasc/biopet/pipelines/flexiprep/scripts/Seqstat.scala
@@ -2,6 +2,7 @@ package nl.lumc.sasc.biopet.pipelines.flexiprep.scripts
 
 import java.io.File
 
+import nl.lumc.sasc.biopet.function.fastq.Fastqc
 import org.broadinstitute.gatk.utils.commandline.{ Input, Output }
 
 import nl.lumc.sasc.biopet.core.config.Configurable
@@ -16,9 +17,23 @@ class Seqstat(val root: Configurable) extends PythonCommandLineFunction {
 
   @Output(doc = "Output file", shortName = "out", required = true)
   var out: File = _
-
+  
   var fmt: String = _
-
+  var fastqc: Fastqc = _
+  
+  override def beforeCmd {
+    if (fastqc != null && fmt == null) {
+      fastqc.getEncoding match {
+        case null => {}
+        case s if (s.contains("Sanger / Illumina 1.9")) => fmt = "sanger"
+        case s if (s.contains("Illumina <1.3")) => fmt = "solexa"
+        case s if (s.contains("Illumina 1.3")) => fmt = "illumina"
+        case s if (s.contains("Illumina 1.5")) => fmt = "illumina"
+        //case _ => null
+      }
+    }
+  }
+  
   def cmdLine = {
     getPythonCommand +
       optional("--fmt", fmt) +
