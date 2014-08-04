eac6893d2dafbf685751f11aab670fe29f8f38db
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-08-04 16:26:19 +0200

    Added constructor

--- flexiprep/src/main/scala/nl/lumc/sasc/biopet/pipelines/flexiprep/SeqtkSeq.scala
+++ flexiprep/src/main/scala/nl/lumc/sasc/biopet/pipelines/flexiprep/SeqtkSeq.scala
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package nl.lumc.sasc.biopet.pipelines.flexiprep
 
+import java.io.File
 import nl.lumc.sasc.biopet.core.config.Configurable
 import nl.lumc.sasc.biopet.extensions.Ln
 
@@ -34,5 +29,15 @@ class SeqtkSeq(root: Configurable) extends nl.lumc.sasc.biopet.extensions.seqtk.
       analysisName = getClass.getSimpleName + "-ln"
       Ln(this, input, output).cmd
     }
+  }
+}
+
+object SeqtkSeq {
+  def apply(root: Configurable, input:File, output:File, fastqc:Fastqc = null): SeqtkSeq = {
+    val seqtkSeq = new SeqtkSeq(root)
+    seqtkSeq.input = input
+    seqtkSeq.output = output
+    seqtkSeq.fastqc = fastqc
+    return seqtkSeq
   }
 }
