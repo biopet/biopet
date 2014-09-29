61af28f80802f1a3aa7d80320d92d3e41b08297a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-09-29 17:26:12 +0200

    Added framework for new pipeline

---
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/pipelines/basty/Basty.scala
@@ -0,0 +1,57 @@
+package nl.lumc.sasc.biopet.pipelines.basty
+
+import nl.lumc.sasc.biopet.core.MultiSampleQScript
+import nl.lumc.sasc.biopet.core.PipelineCommand
+import nl.lumc.sasc.biopet.core.config.Configurable
+import nl.lumc.sasc.biopet.pipelines.gatk.GatkPipeline
+import org.broadinstitute.gatk.queue.QScript
+
+class Basty (val root: Configurable) extends QScript with MultiSampleQScript {
+  def this() = this(null)
+  
+  val gatkPipeline = new GatkPipeline(this)
+  
+  class LibraryOutput extends AbstractLibraryOutput {
+  }
+  
+  class SampleOutput extends AbstractSampleOutput {
+  }
+  
+  def init() {
+    gatkPipeline.outputDir = outputDir
+    gatkPipeline.init
+  }
+  
+  def biopetScript() {
+    gatkPipeline.biopetScript
+    addAll(gatkPipeline.functions)
+    
+    runSamplesJobs()
+  }
+  
+  // Called for each sample
+  def runSingleSampleJobs(sampleConfig: Map[String, Any]): SampleOutput = {
+    val sampleOutput = new SampleOutput
+    val sampleID: String = sampleConfig("ID").toString
+    val sampleDir = globalSampleDir + sampleID
+        
+    sampleOutput.libraries = runLibraryJobs(sampleConfig)
+    
+    return sampleOutput
+  }
+  
+  // Called for each run from a sample
+  def runSingleLibraryJobs(runConfig: Map[String, Any], sampleConfig: Map[String, Any]): LibraryOutput = {
+    val libraryOutput = new LibraryOutput
+    
+    val runID: String = runConfig("ID").toString
+    val sampleID: String = sampleConfig("ID").toString
+    val runDir: String = globalSampleDir + sampleID + "/run_" + runID + "/"
+    
+    return libraryOutput
+  }
+}
+
+object Basty extends PipelineCommand {
+  override val pipeline = "/nl/lumc/sasc/biopet/pipelines/basty/Basty.class"  
+}
