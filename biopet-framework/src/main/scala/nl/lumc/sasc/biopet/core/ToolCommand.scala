13da13e9a6ab5bb4f8f43bbfb901ffc84f295076
bow <bow@bow.web.id>
2014-10-07 14:17:21 +0200

    Simplify ToolCommand.toolName value

--- biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/ToolCommand.scala
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/ToolCommand.scala
@@ -3,10 +3,10 @@ package nl.lumc.sasc.biopet.core
 import org.broadinstitute.gatk.queue.util.Logging
 
 
-abstract trait ToolCommand extends Logging {
+trait ToolCommand extends Logging {
 
-  lazy val toolName = this.getClass.getName
-    .split("\\$").last.split("\\.").last
+  lazy val toolName = this.getClass.getSimpleName
+    .split("\\$").last
 
   def main(args: Array[String])
 }
