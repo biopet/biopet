f3ea203e944a4ad2c523b9a6f709da17acb7e889
bow <bow@bow.web.id>
2014-10-22 16:16:57 +0200

    Update tests

--- biopet-framework/src/test/scala/nl/lumc/sasc/biopet/tools/ExtractAlignedFastqUnitTest.scala
+++ biopet-framework/src/test/scala/nl/lumc/sasc/biopet/tools/ExtractAlignedFastqUnitTest.scala
@@ -14,13 +14,41 @@ class ExtractAlignedFastqUnitTest extends TestNGSuite with Matchers {
   import ExtractAlignedFastq._
 
   @Test def testIntervalStartEnd() = {
-    val ivs = List("chr5:1000-1100")
-    val obs = makeFeatureFromString(ivs).next()
+    val obs = makeFeatureFromString(List("chr5:1000-1100")).next()
     val exp = new BasicFeature("chr5", 1000, 1100)
     obs.getChr should === (exp.getChr)
     obs.getStart should === (exp.getStart)
     obs.getEnd should === (exp.getEnd)
   }
 
+  @Test def testIntervalStartEndComma() = {
+    val obs = makeFeatureFromString(List("chr5:1,000-1,100")).next()
+    val exp = new BasicFeature("chr5", 1000, 1100)
+    obs.getChr should === (exp.getChr)
+    obs.getStart should === (exp.getStart)
+    obs.getEnd should === (exp.getEnd)
+  }
+
+  @Test def testIntervalStartEndDot() = {
+    val obs = makeFeatureFromString(List("chr5:1.000-1.100")).next()
+    val exp = new BasicFeature("chr5", 1000, 1100)
+    obs.getChr should === (exp.getChr)
+    obs.getStart should === (exp.getStart)
+    obs.getEnd should === (exp.getEnd)
+  }
+
+  @Test def testIntervalStart() = {
+    val obs = makeFeatureFromString(List("chr5:1000")).next()
+    val exp = new BasicFeature("chr5", 1000, 1000)
+    obs.getChr should === (exp.getChr)
+    obs.getStart should === (exp.getStart)
+    obs.getEnd should === (exp.getEnd)
+  }
+
+  @Test def testIntervalError() = {
+    intercept[IllegalArgumentException] {
+      makeFeatureFromString(List("chr5:1000-")).next()
+    }
+  }
 }
 
