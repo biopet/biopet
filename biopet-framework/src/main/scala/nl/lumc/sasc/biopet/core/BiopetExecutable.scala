bd3cc3d3c9d489f541aa05c1965ab0e4279eeb06
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-10-24 14:00:52 +0200

    Added BedToInterval to exe

--- biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/BiopetExecutable.scala
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/BiopetExecutable.scala
@@ -26,7 +26,8 @@ object BiopetExecutable {
       nl.lumc.sasc.biopet.tools.CheckAllelesVcfInBam,
       nl.lumc.sasc.biopet.tools.VcfToTsv,
       nl.lumc.sasc.biopet.tools.VcfFilter,
-      nl.lumc.sasc.biopet.tools.FindRepeatsPacBio)
+      nl.lumc.sasc.biopet.tools.FindRepeatsPacBio,
+      nl.lumc.sasc.biopet.tools.BedToInterval)
   )
   
   /**
