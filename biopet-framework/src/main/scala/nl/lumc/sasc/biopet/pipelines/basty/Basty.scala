038c3e4a5d5b9a130467e139f78919de7c2df4b0
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-10-30 17:03:03 +0100

    Added BastyGenerateFasta

--- biopet-framework/src/main/scala/nl/lumc/sasc/biopet/pipelines/basty/Basty.scala
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/pipelines/basty/Basty.scala
@@ -3,7 +3,9 @@ package nl.lumc.sasc.biopet.pipelines.basty
 import nl.lumc.sasc.biopet.core.MultiSampleQScript
 import nl.lumc.sasc.biopet.core.PipelineCommand
 import nl.lumc.sasc.biopet.core.config.Configurable
+import nl.lumc.sasc.biopet.extensions.Cat
 import nl.lumc.sasc.biopet.pipelines.gatk.GatkPipeline
+import nl.lumc.sasc.biopet.tools.BastyGenerateFasta
 import org.broadinstitute.gatk.queue.QScript
 
 class Basty(val root: Configurable) extends QScript with MultiSampleQScript {
@@ -13,6 +15,8 @@ class Basty(val root: Configurable) extends QScript with MultiSampleQScript {
   }
 
   class SampleOutput extends AbstractSampleOutput {
+    var outputVariants: File = _
+    var outputVariantsSnps: File = _
   }
 
   defaults ++= Map("ploidy" -> 1, "use_haplotypecaller" -> false, "use_unifiedgenotyper" -> true)
@@ -29,16 +33,25 @@ class Basty(val root: Configurable) extends QScript with MultiSampleQScript {
     gatkPipeline.biopetScript
     addAll(gatkPipeline.functions)
 
+    val refVariants = addGenerateFasta("Reference", outputDir + "reference/")
+    val refVariantSnps = addGenerateFasta("Reference", outputDir + "reference/", snpsOnly = true)
+
     runSamplesJobs()
+
+    add(Cat(this, refVariants :: samplesOutput.map(_._2.outputVariants).toList, outputDir + "fastas/variant.fasta"))
+    add(Cat(this, refVariantSnps :: samplesOutput.map(_._2.outputVariantsSnps).toList, outputDir + "fastas/variant.snps_only.fasta"))
   }
 
   // Called for each sample
   def runSingleSampleJobs(sampleConfig: Map[String, Any]): SampleOutput = {
     val sampleOutput = new SampleOutput
     val sampleID: String = sampleConfig("ID").toString
-    val sampleDir = globalSampleDir + sampleID
+    val sampleDir = globalSampleDir + sampleID + "/"
 
     sampleOutput.libraries = runLibraryJobs(sampleConfig)
+
+    sampleOutput.outputVariants = addGenerateFasta(sampleID, sampleDir)
+    sampleOutput.outputVariantsSnps = addGenerateFasta(sampleID, sampleDir, snpsOnly = true)
 
     return sampleOutput
   }
@@ -52,6 +65,16 @@ class Basty(val root: Configurable) extends QScript with MultiSampleQScript {
     val runDir: String = globalSampleDir + sampleID + "/run_" + runID + "/"
 
     return libraryOutput
+  }
+
+  def addGenerateFasta(sampleName: String, outputDir: String, snpsOnly: Boolean = false): File = {
+    val bastyGenerateFasta = new BastyGenerateFasta(this)
+    bastyGenerateFasta.inputVcf = gatkPipeline.multisampleVariantcalling.scriptOutput.finalVcfFile
+    bastyGenerateFasta.outputVariants = outputDir + sampleName + ".variants" + (if (snpsOnly) ".snps_only" else "") + ".fasta"
+    bastyGenerateFasta.sampleName = sampleName
+    bastyGenerateFasta.snpsOnly = snpsOnly
+    add(bastyGenerateFasta)
+    return bastyGenerateFasta.outputVariants
   }
 }
 
