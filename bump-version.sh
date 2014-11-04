a547788ff6269624716b61739d86e22434d0df35
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-11-04 15:35:56 +0100

    No version backup

--- bump-version.sh
+++ bump-version.sh
@@ -2,6 +2,5 @@
 
 DIR=`readlink -f \`dirname $0\``
 
-
-mvn -f $DIR/pom.xml versions:set -DnewVersion=$1
-mvn -f $DIR/biopet-framework/pom.xml versions:set -DnewVersion=$1
+mvn -f $DIR/pom.xml versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
+mvn -f $DIR/biopet-framework/pom.xml versions:set -DnewVersion=$1 -DgenerateBackupPoms=false
