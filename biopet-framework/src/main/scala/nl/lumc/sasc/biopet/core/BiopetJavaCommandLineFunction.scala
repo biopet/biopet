ffc8be9b4f73080fa3072e90fc5307c059332cc6
sajvanderzeeuw <sajvanderzeeuw@sasc-sander.lumcnet.prod.intern>
2014-11-07 16:53:06 +0100

    GC options can now be set through the config

--- biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/BiopetJavaCommandLineFunction.scala
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/BiopetJavaCommandLineFunction.scala
@@ -5,6 +5,10 @@ import org.broadinstitute.gatk.queue.function.JavaCommandLineFunction
 trait BiopetJavaCommandLineFunction extends JavaCommandLineFunction with BiopetCommandLineFunctionTrait {
   executable = "java"
 
+  javaGCThreads = config("java_gc_threads")
+  javaGCHeapFreeLimit = config("java_gc_heap_freelimit")
+  javaGCTimeLimit = config("java_gc_timelimit")
+
   override def afterGraph {
     memoryLimit = config("memory_limit")
   }
