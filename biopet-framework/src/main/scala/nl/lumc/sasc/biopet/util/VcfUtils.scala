0886c76f3c8a5177fcee354133226f20b8f3fc68
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-11-11 15:00:59 +0100

    Added general vcf utils

---
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/util/VcfUtils.scala
@@ -0,0 +1,19 @@
+package nl.lumc.sasc.biopet.util
+
+import htsjdk.variant.variantcontext.VariantContext
+import scala.collection.JavaConversions._
+
+/** Utility object for general vcf file/records functions. */
+object VcfUtils {
+  /**
+   * Return longest allele of VariantContext.
+   *
+   * @param vcfRecord record to check
+   * @return allele with most nucleotides 
+   */
+  def getLongestAllele(vcfRecord: VariantContext) = {
+    val alleles = vcfRecord.getAlleles
+    val longestAlleleId = alleles.map(_.getBases.length).zipWithIndex.maxBy(_._1)._2
+    alleles(longestAlleleId)
+  }
+}
