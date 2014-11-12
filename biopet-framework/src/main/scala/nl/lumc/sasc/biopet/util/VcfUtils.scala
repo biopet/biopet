53855f50b428385da3a08027db59c7b4374c6587
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-11-12 12:59:43 +0100

    Added fillAllele function

--- biopet-framework/src/main/scala/nl/lumc/sasc/biopet/util/VcfUtils.scala
+++ biopet-framework/src/main/scala/nl/lumc/sasc/biopet/util/VcfUtils.scala
@@ -9,11 +9,15 @@ object VcfUtils {
    * Return longest allele of VariantContext.
    *
    * @param vcfRecord record to check
-   * @return allele with most nucleotides 
+   * @return allele with most nucleotides
    */
   def getLongestAllele(vcfRecord: VariantContext) = {
     val alleles = vcfRecord.getAlleles
     val longestAlleleId = alleles.map(_.getBases.length).zipWithIndex.maxBy(_._1)._2
     alleles(longestAlleleId)
+  }
+
+  def fillAllele(bases: String, newSize: Int, fillWith: Char = 'N'): String = {
+    bases + (Array.fill[Char](newSize - bases.size)(fillWith)).mkString
   }
 }
