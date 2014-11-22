6441dfd056571ae7b0f2462a22a23b782261ef88
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2014-11-22 15:29:08 +0100

    Added protected artifact for gatk extensions

---
+++ protected/biopet-gatk-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/gatk/PrintReads.scala
@@ -0,0 +1,21 @@
+package nl.lumc.sasc.biopet.extensions.gatk
+
+import java.io.File
+import nl.lumc.sasc.biopet.core.config.Configurable
+
+class PrintReads(val root: Configurable) extends org.broadinstitute.gatk.queue.extensions.gatk.PrintReads with GatkGeneral {
+  memoryLimit = Option(4)
+
+  override val defaultVmem = "8G"
+
+  if (config.contains("scattercount")) scatterCount = config("scattercount")
+}
+
+object PrintReads {
+  def apply(root: Configurable, input: File, output: File): PrintReads = {
+    val br = new PrintReads(root)
+    br.input_file :+= input
+    br.out = output
+    return br
+  }
+}
