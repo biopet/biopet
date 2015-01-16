768d77cc69a616ab1540ea197ba15fba4cfc1a41
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-01-16 10:26:09 +0100

    Added sub path instead of submodule

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/extensions/samtools/Samtools.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/extensions/samtools/Samtools.scala
@@ -18,7 +18,8 @@ package nl.lumc.sasc.biopet.extensions.samtools
 import nl.lumc.sasc.biopet.core.BiopetCommandLineFunction
 
 abstract class Samtools extends BiopetCommandLineFunction {
-  executable = config("exe", default = "samtools", submodule = "samtools", freeVar = false)
+  override def subPath = "samtools" :: super.subPath
+  executable = config("exe", default = "samtools", freeVar = false)
   override def versionCommand = executable
   override val versionRegex = """Version: (.*)""".r
   override val versionExitcode = List(0, 1)
