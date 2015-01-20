5267fdf954d3c20915abcd8df02adfb52f84606a
Sander van der Zeeuw <s.a.j.van_der_zeeuw@lumc.nl>
2015-01-20 17:03:35 +0100

    Changes in format of heatmap

--- public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotHeatmap.R
+++ public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotHeatmap.R
@@ -8,17 +8,17 @@ outputArgClustering <- args[3]
 col <- heat.colors(250)
 col[250] <- "#00FF00"
 
-heat<-read.table(inputArg, header = 1, sep= '\t')
+heat<-read.table(inputArg, header = 1, sep= '\t', stringsAsFactors = F)
 rownames(heat) <- heat[,1]
 heat<- heat[,-1]
 
 heat<- as.matrix(heat)
 
-png(file = outputArg, width = 1000, height = 1000)
-heatmap.2(heat, trace = 'none', col = col, Colv=NA, Rowv=NA, dendrogram="none")
+png(file = outputArg, width = 1500, height = 1500)
+heatmap.2(heat, trace = 'none', col = col, Colv=NA, Rowv=NA, dendrogram="none", margins = c(10, 10))
 dev.off()
 
 
-png(file = outputArgClustering, width = 1000, height = 1000)
-heatmap.2(heat, trace = 'none', col = col, Colv="Rowv", dendrogram="row")
+png(file = outputArgClustering, width = 1500, height = 1500)
+heatmap.2(heat, trace = 'none', col = col, Colv="Rowv", dendrogram="row",margins = c(10, 10))
 dev.off()
