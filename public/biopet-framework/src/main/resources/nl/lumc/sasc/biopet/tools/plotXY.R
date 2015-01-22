1a782ccf0cd790ac23e35636000aca19997142ac
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-01-22 16:56:16 +0100

    Fix xy plot

--- public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotXY.R
+++ public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotXY.R
@@ -1,4 +1,3 @@
-library('gplots')
 library('ggplot2')
 library('reshape2')
 
@@ -6,23 +5,13 @@ args <- commandArgs(TRUE)
 inputArg <- args[1]
 outputArg <- args[2]
 
-print(inputArg)
-print(outputArg)
-
 tsv<-read.table(inputArg, header = 1, sep= '\t', stringsAsFactors = F)
 
-print(inputArg)
-print(outputArg)
-
 data <- melt(tsv)
-#colnames(data) <- c("x", "sample", "value")
 
 data$X <- as.numeric(data$X)
 data <- na.omit(data)
 data <- data[data$value > 0,]
-
-print(inputArg)
-print(outputArg)
 
 print("Starting to plot")
 png(file = outputArg, width = 1500, height = 1500)
