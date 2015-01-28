d64c15aa3a3abe9c6da62eb2ac959ea1ca7f5b43
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-01-28 14:43:43 +0100

    Change color on heatmap

--- public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotHeatmap.R
+++ public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotHeatmap.R
@@ -1,24 +1,25 @@
 library('gplots')
+library('RColorBrewer')
 
 args <- commandArgs(TRUE)
 inputArg <- args[1]
 outputArg <- args[2]
 outputArgClustering <- args[3]
 
-col <- heat.colors(250)
-col[250] <- "#00FF00"
+col <- colorRampPalette(brewer.pal(12, "Spectral"))(50)
 
 heat<-read.table(inputArg, header = 1, sep= '\t', stringsAsFactors = F)
+heat[heat==1] <- NA
 rownames(heat) <- heat[,1]
 heat<- heat[,-1]
 
 heat<- as.matrix(heat)
 
 png(file = outputArg, width = 1500, height = 1500)
-heatmap.2(heat, trace = 'none', col = col, Colv=NA, Rowv=NA, dendrogram="none", margins = c(10, 10))
+heatmap.2(heat, trace = 'none', col = col, Colv=NA, Rowv=NA, dendrogram="none", margins = c(10, 10), na.color="#00FF00")
 dev.off()
 
 
 png(file = outputArgClustering, width = 1500, height = 1500)
-heatmap.2(heat, trace = 'none', col = col, Colv="Rowv", dendrogram="row",margins = c(10, 10))
+heatmap.2(heat, trace = 'none', col = col, Colv="Rowv", dendrogram="row",margins = c(10, 10), na.color="#00FF00")
 dev.off()
