5dc0f7a4ab34d0ee29644cd96e8ec7d32adf08a8
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-01-29 14:58:11 +0100

    Change color scale to fix clustering and added a dendrogram plot

--- public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotHeatmap.R
+++ public/biopet-framework/src/main/resources/nl/lumc/sasc/biopet/tools/plotHeatmap.R
@@ -5,20 +5,30 @@ args <- commandArgs(TRUE)
 inputArg <- args[1]
 outputArg <- args[2]
 outputArgClustering <- args[3]
+outputArgDendrogram <- args[4]
 
-col <- colorRampPalette(brewer.pal(12, "Spectral"))(50)
 
 heat<-read.table(inputArg, header = 1, sep= '\t', stringsAsFactors = F)
-heat[heat==1] <- NA
+#heat[heat==1] <- NA
 rownames(heat) <- heat[,1]
 heat<- heat[,-1]
-
 heat<- as.matrix(heat)
+
+colNumber <- 50
+col <- colorRampPalette(brewer.pal(12, "Spectral"))(colNumber)
+for (i in (colNumber+1):(colNumber+round((dist(range(heat)) - dist(range(heat[heat < 1]))) / dist(range(heat[heat < 1])) * colNumber))) {
+    col[i] <- col[colNumber]
+}
+col[length(col)] <- "#00FF00"
 
 png(file = outputArg, width = 1200, height = 1200)
 heatmap.2(heat, trace = 'none', col = col, Colv=NA, Rowv=NA, dendrogram="none", margins = c(12, 12), na.color="#00FF00")
 dev.off()
 
+hc <- hclust(d = dist(heat))
+png(file = outputArgDendrogram, width = 1200, height = 1200)
+plot(as.dendrogram(hc), horiz=TRUE, asp=0.02)
+dev.off()
 
 png(file = outputArgClustering, width = 1200, height = 1200)
 heatmap.2(heat, trace = 'none', col = col, Colv="Rowv", dendrogram="row",margins = c(12, 12), na.color="#00FF00")
