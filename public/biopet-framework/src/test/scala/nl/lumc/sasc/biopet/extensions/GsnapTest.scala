5fb1125cd58dd75827d0b3c375b0d7203dc07e8a
bow <bow@bow.web.id>
2015-02-06 16:34:06 +0100

    Update GSNAP wrapper test

--- public/biopet-framework/src/test/scala/nl/lumc/sasc/biopet/extensions/GsnapTest.scala
+++ public/biopet-framework/src/test/scala/nl/lumc/sasc/biopet/extensions/GsnapTest.scala
@@ -11,9 +11,20 @@ import org.scalatest.testng.TestNGSuite
 import org.testng.SkipException
 import org.testng.annotations.{ BeforeClass, Test }
 
+import nl.lumc.sasc.biopet.core.config.Config
+
 class GsnapTest extends TestNGSuite with Matchers {
 
+  private def setConfig(key: String, value: String): Map[String, Any] = {
+    val oldMap: Map[String, Any] = Config.global.map.toMap
+    Config.global.map += (key -> value)
+    oldMap
+  }
+
+  private def restoreConfig(oldMap: Map[String, Any]): Unit = Config.global.map = oldMap
+
   @BeforeClass def checkExecutable() = {
+    val oldMap = setConfig("db", "mock")
     val wrapper = new Gsnap(null)
     val proc = Process(wrapper.versionCommand)
     val exitCode =
@@ -26,8 +37,13 @@ class GsnapTest extends TestNGSuite with Matchers {
       }
     if (exitCode != 0)
       throw new SkipException("Skipping GSNAP test because the executable can not be found")
+    restoreConfig(oldMap)
   }
 
   @Test(description = "GSNAP version number capture from executable")
-  def testVersion() = new Gsnap(null).getVersion should not be "N/A"
+  def testVersion() = {
+    val oldMap = setConfig("db", "mock")
+    new Gsnap(null).getVersion should not be "N/A"
+    restoreConfig(oldMap)
+  }
 }
