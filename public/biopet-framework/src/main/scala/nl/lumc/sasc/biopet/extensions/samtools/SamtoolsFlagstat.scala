e86f6183a48dd587764e23740fd91188199eae4f
bow <bow@bow.web.id>
2015-02-17 18:51:40 +0100

    Fix cryptic pipeline errors caused by using directory as output files

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/extensions/samtools/SamtoolsFlagstat.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/extensions/samtools/SamtoolsFlagstat.scala
@@ -30,17 +30,11 @@ class SamtoolsFlagstat(val root: Configurable) extends Samtools {
 }
 
 object SamtoolsFlagstat {
-  def apply(root: Configurable, input: File, output: File): SamtoolsFlagstat = {
+  def apply(root: Configurable, input: File, outputDir: File): SamtoolsFlagstat = {
     val flagstat = new SamtoolsFlagstat(root)
     flagstat.input = input
-    flagstat.output = output
-    return flagstat
-  }
-
-  def apply(root: Configurable, input: File, outputDir: String): SamtoolsFlagstat = {
-    val dir = if (outputDir.endsWith("/")) outputDir else outputDir + "/"
-    val outputFile = new File(dir + swapExtension(input.getName))
-    return apply(root, input, outputFile)
+    flagstat.output = new File(outputDir + swapExtension(input.getName))
+    flagstat
   }
 
   def apply(root: Configurable, input: File): SamtoolsFlagstat = {
