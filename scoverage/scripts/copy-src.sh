d170b185e78395614d116056eb4c6c282f919180
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-02-24 15:33:57 +0100

    Added a comment

--- scoverage/scripts/copy-src.sh
+++ scoverage/scripts/copy-src.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script copy all src dirs from all projects to the target dir before. This way we get a agregated test coverage report.
+
 my_dir="$(dirname "$0")"
 
 mkdir -p $my_dir/../target/src/
