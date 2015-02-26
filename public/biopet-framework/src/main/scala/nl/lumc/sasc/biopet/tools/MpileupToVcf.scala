bde80bda1f2cf8902d151e8675f785030488a1d5
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-02-26 11:13:53 +0100

    Add automatic sample detection

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/tools/MpileupToVcf.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/tools/MpileupToVcf.scala
@@ -17,6 +17,7 @@ package nl.lumc.sasc.biopet.tools
 
 import java.io.File
 import java.io.PrintWriter
+import htsjdk.samtools.SamReaderFactory
 import nl.lumc.sasc.biopet.core.BiopetJavaCommandLineFunction
 import nl.lumc.sasc.biopet.core.ToolCommand
 import nl.lumc.sasc.biopet.core.config.Configurable
@@ -57,6 +58,16 @@ class MpileupToVcf(val root: Configurable) extends BiopetJavaCommandLineFunction
   override def beforeGraph {
     super.beforeGraph
     val samtoolsMpileup = new SamtoolsMpileup(this)
+  }
+
+  override def beforeCmd: Unit = {
+    if (sample == null && inputBam.exists()) {
+      val inputSam = SamReaderFactory.makeDefault.open(inputBam)
+      val readGroups = inputSam.getFileHeader.getReadGroups
+      val samples = readGroups.map(readGroup => readGroup.getSample).distinct
+      sample = samples.head
+      inputSam.close
+    }
   }
 
   override def commandLine = {
