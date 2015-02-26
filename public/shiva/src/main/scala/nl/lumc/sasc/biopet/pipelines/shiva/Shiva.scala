5aae754efd3df8dc1adbfb669d4c4a32a29ad04e
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-02-26 09:26:48 +0100

    Remove unused imports

--- public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/Shiva.scala
+++ public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/Shiva.scala
@@ -1,14 +1,8 @@
 package nl.lumc.sasc.biopet.pipelines.shiva
 
-import htsjdk.samtools.SamReaderFactory
 import nl.lumc.sasc.biopet.core.config.Configurable
-import nl.lumc.sasc.biopet.core.summary.SummaryQScript
-import nl.lumc.sasc.biopet.core.{MultiSampleQScript, PipelineCommand}
-import nl.lumc.sasc.biopet.extensions.Ln
-import nl.lumc.sasc.biopet.extensions.picard.{AddOrReplaceReadGroups, SamToFastq, MarkDuplicates}
-import nl.lumc.sasc.biopet.pipelines.mapping.Mapping
+import nl.lumc.sasc.biopet.core.PipelineCommand
 import org.broadinstitute.gatk.queue.QScript
-import scala.collection.JavaConversions._
 
 /**
  * Created by pjvan_thof on 2/24/15.
