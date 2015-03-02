335563b98b0788cfb734eb86cb54dcb888d7c8f4
bow <bow@bow.web.id>
2015-03-02 10:42:20 +0100

    Add MergeTables to main tool executables list

--- public/biopet-public-package/src/main/scala/nl/lumc/sasc/biopet/core/BiopetExecutablePublic.scala
+++ public/biopet-public-package/src/main/scala/nl/lumc/sasc/biopet/core/BiopetExecutablePublic.scala
@@ -30,6 +30,7 @@ object BiopetExecutablePublic extends BiopetExecutable {
   )
 
   def tools: List[MainCommand] = List(
+    nl.lumc.sasc.biopet.tools.MergeTables,
     nl.lumc.sasc.biopet.tools.WipeReads,
     nl.lumc.sasc.biopet.tools.ExtractAlignedFastq,
     nl.lumc.sasc.biopet.tools.FastqSync,
