780ab2df2597036c5b9a027b0fe6b2d6980748b5
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-03-09 11:55:33 +0100

    Fix error on zipped input without flexiprep

--- public/mapping/src/test/scala/nl/lumc/sasc/biopet/pipelines/mapping/MappingTest.scala
+++ public/mapping/src/test/scala/nl/lumc/sasc/biopet/pipelines/mapping/MappingTest.scala
@@ -37,18 +37,23 @@ class MappingTest extends TestNGSuite with Matchers {
     val chunks = Array(1, 5, 10, 100)
     val skipMarkDuplicates = Array(true, false)
     val skipFlexipreps = Array(true, false)
+    val zipped = Array(true, false)
 
     for (
       aligner <- aligners;
       pair <- paired;
       chunk <- chunks;
       skipMarkDuplicate <- skipMarkDuplicates;
-      skipFlexiprep <- skipFlexipreps
-    ) yield Array(aligner, pair, chunk, skipMarkDuplicate, skipFlexiprep)
+      skipFlexiprep <- skipFlexipreps;
+      zipped <- zipped
+    ) yield Array(aligner, pair, chunk, skipMarkDuplicate, skipFlexiprep, zipped)
   }
 
   @Test(dataProvider = "mappingOptions")
-  def testMapping(aligner: String, paired: Boolean, chunks: Int, skipMarkDuplicate: Boolean, skipFlexiprep: Boolean) = {
+  def testMapping(aligner: String, paired: Boolean, chunks: Int,
+                  skipMarkDuplicate: Boolean,
+                  skipFlexiprep: Boolean,
+                  zipped: Boolean) = {
     val map = ConfigUtils.mergeMaps(Map("output_dir" -> MappingTest.outputDir,
       "aligner" -> aligner,
       "number_chunks" -> chunks,
@@ -57,15 +62,20 @@ class MappingTest extends TestNGSuite with Matchers {
     ), Map(MappingTest.executables.toSeq: _*))
     val mapping: Mapping = initPipeline(map)
 
-    mapping.input_R1 = new File(mapping.outputDir, "bla_R1.fq")
-    if (paired) mapping.input_R2 = Some(new File(mapping.outputDir, "bla_R2.fq"))
+    if (zipped) {
+      mapping.input_R1 = new File(mapping.outputDir, "bla_R1.fq.gz")
+      if (paired) mapping.input_R2 = Some(new File(mapping.outputDir, "bla_R2.fq.gz"))
+    } else {
+      mapping.input_R1 = new File(mapping.outputDir, "bla_R1.fq")
+      if (paired) mapping.input_R2 = Some(new File(mapping.outputDir, "bla_R2.fq"))
+    }
     mapping.sampleId = Some("1")
     mapping.libId = Some("1")
     mapping.script()
 
     //Flexiprep
     mapping.functions.count(_.isInstanceOf[Fastqc]) shouldBe (if (skipFlexiprep) 0 else if (paired) 4 else 2)
-    mapping.functions.count(_.isInstanceOf[Zcat]) shouldBe 0
+    mapping.functions.count(_.isInstanceOf[Zcat]) shouldBe (if (!zipped || (chunks > 1 && skipFlexiprep)) 0 else if (paired) 2 else 1)
     mapping.functions.count(_.isInstanceOf[Seqstat]) shouldBe ((if (skipFlexiprep) 0 else if (paired) 4 else 2) * chunks)
     mapping.functions.count(_.isInstanceOf[SeqtkSeq]) shouldBe ((if (skipFlexiprep) 0 else if (paired) 2 else 1) * chunks)
     mapping.functions.count(_.isInstanceOf[Cutadapt]) shouldBe ((if (skipFlexiprep) 0 else if (paired) 2 else 1) * chunks)
