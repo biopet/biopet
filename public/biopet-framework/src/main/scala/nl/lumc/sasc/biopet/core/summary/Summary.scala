aa506e53a157a0f386264d43ca4224b771bed278
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-03-30 13:52:02 +0200

    Added Summary class for easy access summary values

---
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/summary/Summary.scala
@@ -0,0 +1,32 @@
+package nl.lumc.sasc.biopet.core.summary
+
+import java.io.File
+
+import nl.lumc.sasc.biopet.utils.ConfigUtils
+
+/**
+ * Created by pjvan_thof on 3/26/15.
+ */
+class Summary(file: File) {
+  val map = ConfigUtils.fileToConfigMap(file)
+
+  lazy val samples: Set[String] = {
+    ConfigUtils.getValueFromPath(map, List("samples")) match {
+      case Some(samples) => ConfigUtils.any2map(samples).keySet
+      case _             => Set()
+    }
+  }
+
+  lazy val libraries: Map[String, Set[String]] = {
+    (for (sample <- samples) yield sample -> {
+      ConfigUtils.getValueFromPath(map, List("samples", sample, "libraries")) match {
+        case Some(libs) => ConfigUtils.any2map(libs).keySet
+        case _          => Set[String]()
+      }
+    }).toMap
+  }
+
+  def getValue(path: List[String]): Option[Any] = {
+    ConfigUtils.getValueFromPath(map, path)
+  }
+}
