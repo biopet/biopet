f8f34f350ff11cb24b24dfc076d9af753ef99140
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-04-06 09:57:47 +0200

    Format fix

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/MultisampleReportBuilder.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/MultisampleReportBuilder.scala
@@ -8,21 +8,21 @@ import nl.lumc.sasc.biopet.core.summary.Summary
 trait MultisampleReportBuilder extends ReportBuilder {
   def generalPage: ReportPage
 
-  def samplePage(sampleId: String, args: Map[String, Any]) : ReportPage
+  def samplePage(sampleId: String, args: Map[String, Any]): ReportPage
 
-  def libraryPage(libraryId: String, args: Map[String, Any]) : ReportPage
+  def libraryPage(libraryId: String, args: Map[String, Any]): ReportPage
 
-  def generateSamplesPage(args: Map[String, Any]) : ReportPage = {
+  def generateSamplesPage(args: Map[String, Any]): ReportPage = {
     val samplePages = summary.samples
       .map(sampleId => (sampleId -> samplePage(sampleId, args ++ Map("sampleId" -> Some(sampleId)))))
       .toMap
     ReportPage(samplePages, Map(), args)
   }
 
-  def generateLibraryPage(args: Map[String, Any]) : ReportPage = {
+  def generateLibraryPage(args: Map[String, Any]): ReportPage = {
     val libPages = summary.libraries(args("sampleId") match {
       case Some(x) => x.toString
-      case None => throw new IllegalStateException("Sample not found")
+      case None    => throw new IllegalStateException("Sample not found")
     })
       .map(libId => (libId -> libraryPage(libId, args ++ Map("libId" -> Some(libId)))))
       .toMap
