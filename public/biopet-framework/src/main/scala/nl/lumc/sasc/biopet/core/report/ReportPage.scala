0e52912145dc92e0d91cf79af5002c66cb112c98
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-04-06 09:51:04 +0200

    Change URL to string

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/ReportPage.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/ReportPage.scala
@@ -6,5 +6,5 @@ import java.net.URL
  * Created by pjvan_thof on 3/27/15.
  */
 case class ReportPage(val subPages: Map[String, ReportPage],
-                      val sections: Map[String, URL],
+                      val sections: Map[String, String],
                       val args: Map[String, Any])
