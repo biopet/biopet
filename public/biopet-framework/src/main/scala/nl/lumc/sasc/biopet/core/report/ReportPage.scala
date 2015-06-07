bea78535defc89e58829e31ad2dc94dd490948ce
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-06-07 12:32:35 +0200

    remove unneeded vals

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/ReportPage.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/ReportPage.scala
@@ -3,6 +3,6 @@ package nl.lumc.sasc.biopet.core.report
 /**
  * Created by pjvan_thof on 3/27/15.
  */
-case class ReportPage(val subPages: Map[String, ReportPage],
-                      val sections: List[(String, ReportSection)],
-                      val args: Map[String, Any])
+case class ReportPage(subPages: Map[String, ReportPage],
+                      sections: List[(String, ReportSection)],
+                      args: Map[String, Any])
