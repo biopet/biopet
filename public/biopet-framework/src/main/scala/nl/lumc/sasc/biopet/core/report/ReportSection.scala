7fe670b796feb02cd295ceb4b68e2df77cd57035
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-06-08 15:36:51 +0200

    Added option to add intro

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/ReportSection.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/core/report/ReportSection.scala
@@ -4,9 +4,13 @@ package nl.lumc.sasc.biopet.core.report
  * Created by pjvan_thof on 4/8/15.
  */
 case class ReportSection(location: String,
-                         args: Map[String, Any] = Map()) {
+                         args: Map[String, Any] = Map(),
+                         intro: Option[String] = None) {
 
   def render(args: Map[String, Any]): String = {
-    ReportBuilder.renderTemplate(location, args ++ this.args)
+    (intro match {
+      case Some(template) => ReportBuilder.renderTemplate(location, args ++ this.args)
+      case _              => ""
+    }) + ReportBuilder.renderTemplate(location, args ++ this.args)
   }
 }
