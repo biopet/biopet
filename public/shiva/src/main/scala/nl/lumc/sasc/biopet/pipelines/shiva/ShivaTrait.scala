8e606123e8e8166f05f057270cf716f50f9bfd0b
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-01 14:13:04 +0200

    Adding toucan to shiva

--- public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/ShivaTrait.scala
+++ public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/ShivaTrait.scala
@@ -24,6 +24,7 @@ import nl.lumc.sasc.biopet.extensions.Ln
 import nl.lumc.sasc.biopet.extensions.picard.{ AddOrReplaceReadGroups, MarkDuplicates, SamToFastq }
 import nl.lumc.sasc.biopet.pipelines.bammetrics.BamMetrics
 import nl.lumc.sasc.biopet.pipelines.mapping.Mapping
+import nl.lumc.sasc.biopet.pipelines.toucan.Toucan
 
 import scala.collection.JavaConversions._
 
@@ -292,6 +293,16 @@ trait ShivaTrait extends MultiSampleQScript with SummaryQScript with Reference {
       vc.biopetScript()
       addAll(vc.functions)
       addSummaryQScript(vc)
+
+      if (config("annotation", default = true).asBoolean) {
+        val toucan = new Toucan(this)
+        toucan.outputDir = new File(outputDir, "annotation")
+        toucan.inputVCF = vc.finalFile
+        toucan.init()
+        toucan.biopetScript()
+        addAll(toucan.functions)
+        addSummaryQScript(toucan)
+      }
     })
 
     svCalling.foreach(sv => {
