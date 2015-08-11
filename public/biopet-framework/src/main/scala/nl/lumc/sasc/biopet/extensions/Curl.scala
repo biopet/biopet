982769eb7f54b726a146d8e89904f3ddb6d2f168
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-11 13:35:46 +0200

    Added basic extension for curl

---
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/extensions/Curl.scala
@@ -0,0 +1,23 @@
+package nl.lumc.sasc.biopet.extensions
+
+import java.io.File
+
+import nl.lumc.sasc.biopet.core.BiopetCommandLineFunction
+import nl.lumc.sasc.biopet.core.config.Configurable
+import org.broadinstitute.gatk.utils.commandline.Output
+
+/**
+ * Created by pjvan_thof on 8/11/15.
+ */
+class Curl (val root: Configurable) extends BiopetCommandLineFunction {
+  @Output
+  var output: File = _
+
+  var url: String = _
+
+  executable = config("exe", default = "curl")
+  override def versionCommand = executable + " --version"
+  override def versionRegex = """curl (\w+\.\w+\.\w+) .*""".r
+
+  def cmdLine: String = required(executable) + required(url) + " > " + required(output)
+}
