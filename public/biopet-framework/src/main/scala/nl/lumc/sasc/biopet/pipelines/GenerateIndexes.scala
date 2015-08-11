b648ea62780c867bde8edbb88bd2d1c084ac4ff7
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-11 11:48:56 +0200

    First version of index build pipeline

---
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/pipelines/GenerateIndexes.scala
@@ -0,0 +1,60 @@
+/**
+ * Biopet is built on top of GATK Queue for building bioinformatic
+ * pipelines. It is mainly intended to support LUMC SHARK cluster which is running
+ * SGE. But other types of HPC that are supported by GATK Queue (such as PBS)
+ * should also be able to execute Biopet tools and pipelines.
+ *
+ * Copyright 2014 Sequencing Analysis Support Core - Leiden University Medical Center
+ *
+ * Contact us at: sasc@lumc.nl
+ *
+ * A dual licensing mode is applied. The source code within this project that are
+ * not part of GATK Queue is freely available for non-commercial use under an AGPL
+ * license; For commercial users or users who do not want to follow the AGPL
+ * license, please contact us to obtain a separate license.
+ */
+package nl.lumc.sasc.biopet.pipelines
+
+import java.io.File
+
+import nl.lumc.sasc.biopet.core.{PipelineCommand, BiopetQScript}
+import nl.lumc.sasc.biopet.core.config.Configurable
+import nl.lumc.sasc.biopet.utils.ConfigUtils
+import org.broadinstitute.gatk.queue.QScript
+import org.broadinstitute.gatk.utils.commandline
+
+class GenerateIndexes(val root: Configurable) extends QScript with BiopetQScript {
+  def this() = this(null)
+
+  @Argument
+  var referenceConfigFile: File = _
+
+  var referenceConfig: Map[String, Any] = Map()
+
+  /** This is executed before the script starts */
+  def init(): Unit = {
+    referenceConfig = ConfigUtils.fileToConfigMap(referenceConfigFile)
+  }
+
+  /** Method where jobs must be added */
+  def biopetScript(): Unit = {
+
+    for ((speciesName, c) <- referenceConfig) {
+      val speciesConfig = ConfigUtils.any2map(c)
+      for ((genomeName, c) <- speciesConfig) {
+        val genomeConfig = ConfigUtils.any2map(c)
+        val fastaUrl = genomeConfig.getOrElse("fasta_url",
+          throw new IllegalArgumentException(s"No fasta_url found for $speciesName - $genomeName")).toString
+
+          //TODO: wget fastaUrl
+
+          //TODO: fai
+          //TODO: dict
+
+        //TODO: other indexes
+      }
+    }
+  }
+}
+
+object GenerateIndexes extends PipelineCommand
