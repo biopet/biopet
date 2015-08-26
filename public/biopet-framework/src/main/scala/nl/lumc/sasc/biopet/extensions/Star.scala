0eabe6be44a08d6adcee2b6b1c7f4035dece5293
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-26 13:45:33 +0200

    Raised memory for star

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/extensions/Star.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/extensions/Star.scala
@@ -63,7 +63,7 @@ class Star(val root: Configurable) extends BiopetCommandLineFunction with Refere
   var outFileNamePrefix: String = _
   var runThreadN: Option[Int] = config("runThreadN")
 
-  override def defaultCoreMemory = 4.0
+  override def defaultCoreMemory = 6.0
   override def defaultThreads = 8
 
   /** Sets output files for the graph */
