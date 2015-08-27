63090796e30652406968e3118805222839b1e5e0
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-27 15:31:51 +0200

    Added squish bed extension

--- public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/tools/SquishBed.scala
+++ public/biopet-framework/src/main/scala/nl/lumc/sasc/biopet/tools/SquishBed.scala
@@ -2,12 +2,31 @@ package nl.lumc.sasc.biopet.tools
 
 import java.io.File
 
-import nl.lumc.sasc.biopet.core.ToolCommand
+import nl.lumc.sasc.biopet.core.{ToolCommandFuntion, ToolCommand}
+import nl.lumc.sasc.biopet.core.config.Configurable
 import nl.lumc.sasc.biopet.utils.intervals.BedRecordList
+import org.broadinstitute.gatk.utils.commandline.{Output, Input}
 
 /**
  * Created by pjvanthof on 22/08/15.
  */
+class SquishBed(val root: Configurable) extends ToolCommandFuntion {
+  javaMainClass = getClass.getName
+
+  @Input(doc = "Input Bed file", required = true)
+  var input: File = _
+
+  @Output(doc = "Output interval list", required = true)
+  var output: File = _
+
+  var strandSensitive: Boolean = config("strandSensitive", default = false)
+
+  override def commandLine = super.commandLine +
+    required("-I", input) +
+    required("-o", output) +
+    conditional(strandSensitive, "-s")
+}
+
 object SquishBed extends ToolCommand {
 
   case class Args(input: File = null,
