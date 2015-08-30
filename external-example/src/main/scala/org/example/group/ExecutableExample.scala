dcd0ce92637620a143fe2d907066d54a7165f880
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-30 21:27:21 +0200

    Added example of a biopet executable

---
+++ external-example/src/main/scala/org/example/group/ExecutableExample.scala
@@ -0,0 +1,18 @@
+package org.example.group
+
+import nl.lumc.sasc.biopet.core.{ MainCommand, BiopetExecutable }
+
+/**
+ * Created by pjvanthof on 30/08/15.
+ */
+object ExecutableExample extends BiopetExecutable {
+
+  /** This list defines the pipeline that are usable from the executable */
+  def pipelines: List[MainCommand] = List(
+    org.example.group.pipelines.BiopetPipeline,
+    org.example.group.pipelines.SimplePipeline
+  )
+
+  /** This list defines the (biopet)tools that are usable from the executable */
+  def tools: List[MainCommand] = Nil
+}
