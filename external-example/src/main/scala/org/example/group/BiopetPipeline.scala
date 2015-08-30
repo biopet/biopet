34f97a24ecada591e383764fa6e2e7f4db6368b4
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-08-30 16:49:23 +0200

    Switch to only 1 maven artifact as example

--- external-example/src/main/scala/org/example/group/BiopetPipeline.scala
+++ external-example/src/main/scala/org/example/group/BiopetPipeline.scala
@@ -9,26 +9,36 @@ import org.broadinstitute.gatk.queue.QScript
 /**
  * Created by pjvan_thof on 8/28/15.
  */
-class SinglePipeline(val root: Configurable) extends QScript with SummaryQScript {
+//TODO: Replace class Name
+class BiopetPipeline(val root: Configurable) extends QScript with SummaryQScript {
   def this() = this(null)
 
+  /** Only required when using [[SummaryQScript]] */
   def summaryFile = new File(outputDir, "magpie.summary.json")
 
+  /** Only required when using [[SummaryQScript]] */
   def summaryFiles: Map[String, File] = Map()
 
+  /** Only required when using [[SummaryQScript]] */
   def summarySettings = Map()
 
+  // This method can be used to initialize some classes where needed
   def init(): Unit = {
   }
 
+  // This method is the actual pipeline
   def biopetScript: Unit = {
+
+    // Executing a biopet pipeline inside
     val shiva = new Shiva(this)
     shiva.init()
     shiva.biopetScript()
     addAll(shiva.functions)
 
+    /* Only required when using [[SummaryQScript]] */
     addSummaryQScript(shiva)
   }
 }
 
-object SinglePipeline extends PipelineCommand
+//TODO: Replace object Name, must be the same as the class of the pipeline
+object BiopetPipeline extends PipelineCommand
