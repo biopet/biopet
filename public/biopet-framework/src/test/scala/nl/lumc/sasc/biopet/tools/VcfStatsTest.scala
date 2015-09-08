e119b397fb03f5807d03ee27ccb7ebec18553b8e
Sander Bollen <a.h.b.bollen@lumc.nl>
2015-09-08 16:22:54 +0200

    sort test

--- public/biopet-framework/src/test/scala/nl/lumc/sasc/biopet/tools/VcfStatsTest.scala
+++ public/biopet-framework/src/test/scala/nl/lumc/sasc/biopet/tools/VcfStatsTest.scala
@@ -245,6 +245,17 @@ class VcfStatsTest extends TestNGSuite with Matchers {
   @Test
   def testSortAnyAny = {
     //stub
+    val one: Any = 1
+    val two: Any = 2
+    val text: Any = "hello"
+    val text2 : Any = "goodbye"
+
+    sortAnyAny(one, two) shouldBe true
+    sortAnyAny(two, one) shouldBe false
+    sortAnyAny(text, text2) shouldBe false
+    sortAnyAny(text2, text) shouldBe true
+    sortAnyAny(one, text) shouldBe true
+    sortAnyAny(text, one) shouldBe false
   }
 
   @Test
