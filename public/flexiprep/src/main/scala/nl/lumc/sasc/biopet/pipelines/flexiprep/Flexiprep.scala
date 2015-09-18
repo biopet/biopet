5f7b2cbebd85726c7b4ce5444165cb93d6c0c369
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-09-18 14:03:49 +0200

    Fastq sync is not intermediate

--- public/flexiprep/src/main/scala/nl/lumc/sasc/biopet/pipelines/flexiprep/Flexiprep.scala
+++ public/flexiprep/src/main/scala/nl/lumc/sasc/biopet/pipelines/flexiprep/Flexiprep.scala
@@ -221,6 +221,7 @@ class Flexiprep(val root: Configurable) extends QScript with SummaryQScript with
         fqSync.outputFastq1 = swapExt(outDir, R1, R1_ext, ".sync" + R1_ext)
         fqSync.outputFastq2 = swapExt(outDir, R2.get, R2_ext, ".sync" + R2_ext)
         fqSync.outputStats = swapExt(outDir, R1, R1_ext, ".sync.stats")
+        fqSync.isIntermediate = true
         fqSync.deps :::= deps
         add(fqSync)
         addSummarizable(fqSync, "fastq_sync")
