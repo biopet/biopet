23a98ce37931a5195402f65bf7034c5f1a9fe8b7
Sander Bollen <a.h.b.bollen@lumc.nl>
2015-09-23 18:25:49 +0200

    manwe stub

---
+++ public/biopet-extentsions/src/main/scala/nl/lumc/sasc/biopet/extensions/manwe/Manwe.scala
@@ -0,0 +1,33 @@
+package nl.lumc.sasc.biopet.extensions.manwe
+
+
+import java.io.File
+
+import nl.lumc.sasc.biopet.core.BiopetCommandLineFunction
+import org.broadinstitute.gatk.utils.commandline.Argument
+
+/**
+ * Created by ahbbollen on 23-9-15.
+ * This is python, but not accessed like a script; i.e. called by simply
+ * manwe [subcommand]
+ */
+abstract class Manwe extends BiopetCommandLineFunction {
+  executable = config("exe", default = "manwe", submodule = "manwe")
+
+  override def defaultCoreMemory = 2.0
+  override def defaultThreads = 1
+
+  @Argument(doc="Path to manwe config file containing your database settings", required = true)
+  var manweConfig: Option[File] = config("manwe_config")
+
+  var manweHelp: Boolean = false
+
+  def subCommand : String
+
+  final def cmdLine = {
+    required(executable) +
+    subCommand +
+    required("-c", manweConfig) +
+    conditional(manweHelp, "-h")
+  }
+}
---
+++ public/biopet-extentsions/src/main/scala/nl/lumc/sasc/biopet/extensions/manwe/ManweSamplesList.scala
@@ -0,0 +1,34 @@
+package nl.lumc.sasc.biopet.extensions.manwe
+
+import java.io.File
+
+import org.broadinstitute.gatk.utils.commandline.{Argument, Output}
+
+/**
+ * Created by ahbbollen on 23-9-15.
+ */
+abstract class ManweSamplesList extends Manwe {
+
+  @Output(doc = "output file")
+  var output: File = _
+
+  @Argument(doc = "filter by user URI")
+  var user: Option[String] = None
+
+  @Argument(doc = "filter by group URI")
+  var group: List[String] = Nil
+
+  var onlyPublic: Boolean = false
+
+  def subCommand = {
+    required("samples") + required("list") + optional("-u", user) +
+    repeat("-g", group) + conditional(onlyPublic, "-p") + " > " +
+    required(output)
+  }
+
+
+
+
+
+
+}
