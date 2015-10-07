0cc3590120622289cf3fe89246f64ecdfd2490b9
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-07 10:24:44 +0200

    Format fix

--- public/biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/bowtie/Bowtie.scala
+++ public/biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/bowtie/Bowtie.scala
@@ -17,9 +17,9 @@ package nl.lumc.sasc.biopet.extensions.bowtie
 
 import java.io.File
 
-import nl.lumc.sasc.biopet.core.{BiopetCommandLineFunction, Reference}
+import nl.lumc.sasc.biopet.core.{ BiopetCommandLineFunction, Reference }
 import nl.lumc.sasc.biopet.utils.config.Configurable
-import org.broadinstitute.gatk.utils.commandline.{Input, Output}
+import org.broadinstitute.gatk.utils.commandline.{ Input, Output }
 
 /**
  * Extension for bowtie 1
@@ -41,7 +41,6 @@ class Bowtie(val root: Configurable) extends BiopetCommandLineFunction with Refe
 
   executable = config("exe", default = "bowtie", freeVar = false)
   override def versionRegex = """.*[Vv]ersion:? (.*)""".r
-  override def versionExitcode = List(0, 1)
   override def versionCommand = executable + " --version"
 
   override def defaultCoreMemory = 4.0
@@ -58,37 +57,34 @@ class Bowtie(val root: Configurable) extends BiopetCommandLineFunction with Refe
   var strata: Boolean = config("strata", default = false)
   var maqerr: Option[Int] = config("maqerr")
   var maxins: Option[Int] = config("maxins")
-  var largeIndex: Boolean = config("large-index", default = false)
 
   override def beforeGraph() {
     super.beforeGraph()
     if (reference == null) reference = referenceFasta()
-    val basename = reference.getName.stripSuffix(".fasta").stripSuffix(".fa")
-    if (reference.getParentFile.list().toList.filter(_.startsWith(basename)).exists(_.endsWith(".ebwtl")))
-      largeIndex = config("large-index", default = true)
   }
 
   /** return commandline to execute */
-  def cmdLine = required(executable) +
-    optional("--threads", threads) +
-    conditional(sam, "--sam") +
-    conditional(largeIndex, "--large-index") +
-    conditional(best, "--best") +
-    conditional(strata, "--strata") +
-    optional("--sam-RG", sam_RG) +
-    optional("--seedlen", seedlen) +
-    optional("--seedmms", seedmms) +
-    optional("-k", k) +
-    optional("-m", m) +
-    optional("--maxbts", maxbts) +
-    optional("--maqerr", maqerr) +
-    optional("--maxins", maxins) +
-    required(reference.getAbsolutePath.stripSuffix(".fa").stripSuffix(".fasta")) +
-    (R2 match {
-      case Some(r2) =>
-        required("-1", R1) +
-          optional("-2", r2)
-      case _ => required(R1)
-    }) +
-    " > " + required(output)
+  def cmdLine = {
+    required(executable) +
+      optional("--threads", threads) +
+      conditional(sam, "--sam") +
+      conditional(best, "--best") +
+      conditional(strata, "--strata") +
+      optional("--sam-RG", sam_RG) +
+      optional("--seedlen", seedlen) +
+      optional("--seedmms", seedmms) +
+      optional("-k", k) +
+      optional("-m", m) +
+      optional("--maxbts", maxbts) +
+      optional("--maqerr", maqerr) +
+      optional("--maxins", maxins) +
+      required(reference) +
+      (R2 match {
+        case Some(r2) =>
+          required("-1", R1) +
+            optional("-2", r2)
+        case _ => required(R1)
+      }) +
+      " > " + required(output)
+  }
 }
