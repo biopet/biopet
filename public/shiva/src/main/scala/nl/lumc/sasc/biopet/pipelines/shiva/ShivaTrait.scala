46f8b1b580c79eafd7acb05a6f696da3ff1a80bd
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-21 13:24:25 +0200

    Change to compressed output

--- public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/ShivaTrait.scala
+++ public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/ShivaTrait.scala
@@ -144,8 +144,8 @@ trait ShivaTrait extends MultiSampleQScript with SummaryQScript with Reference {
             config("bam_to_fastq", default = false).asBoolean match {
               case true =>
                 val samToFastq = SamToFastq(qscript, config("bam"),
-                  new File(libDir, sampleId + "-" + libId + ".R1.fastq"),
-                  new File(libDir, sampleId + "-" + libId + ".R2.fastq"))
+                  new File(libDir, sampleId + "-" + libId + ".R1.fq.gz"),
+                  new File(libDir, sampleId + "-" + libId + ".R2.fq.gz"))
                 samToFastq.isIntermediate = true
                 qscript.add(samToFastq)
                 mapping.foreach(mapping => {
