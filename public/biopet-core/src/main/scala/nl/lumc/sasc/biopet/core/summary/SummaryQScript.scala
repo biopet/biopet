974156b37083b566ca71f61c37663b434955efa5
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-28 16:29:42 +0100

    Fixed md5 check

--- public/biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/summary/SummaryQScript.scala
+++ public/biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/summary/SummaryQScript.scala
@@ -116,20 +116,6 @@ trait SummaryQScript extends BiopetQScript { qscript =>
       //TODO: add more checksums types
     }
 
-    for (inputFile <- inputFiles) {
-      inputFile.md5 match {
-        case Some(checksum) => {
-          val checkMd5 = new CheckChecksum
-          checkMd5.inputFile = inputFile.file
-          require(SummaryQScript.md5sumCache.contains(inputFile.file), "Md5 job is not executed, checksum file can't be found")
-          checkMd5.checksumFile = SummaryQScript.md5sumCache(inputFile.file)
-          checkMd5.checksum = checksum
-          add(checkMd5)
-        }
-        case _ =>
-      }
-    }
-
     for ((_, summarizableList) <- summarizables; summarizable <- summarizableList) {
       summarizable match {
         case f: BiopetCommandLineFunction => f.beforeGraph()
@@ -143,6 +129,21 @@ trait SummaryQScript extends BiopetQScript { qscript =>
       summarizable match {
         case f: BiopetJavaCommandLineFunction => if (f.jarFile != null) addChecksum(f.jarFile)
         case _                                =>
+      }
+    }
+
+    for (inputFile <- inputFiles) {
+      inputFile.md5 match {
+        case Some(checksum) => {
+          val checkMd5 = new CheckChecksum
+          checkMd5.inputFile = inputFile.file
+          require(SummaryQScript.md5sumCache.contains(inputFile.file),
+            s"Md5 job is not executed, checksum file can't be found for: ${inputFile.file}")
+          checkMd5.checksumFile = SummaryQScript.md5sumCache(inputFile.file)
+          checkMd5.checksum = checksum
+          add(checkMd5)
+        }
+        case _ =>
       }
     }
 
