3cd8150f738cc6ee95a247dec7b6e42a38228b38
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-10-30 12:56:31 +0100

    Optimize code

--- public/gears/src/main/scala/nl/lumc/sasc/biopet/pipelines/gears/GearsReport.scala
+++ public/gears/src/main/scala/nl/lumc/sasc/biopet/pipelines/gears/GearsReport.scala
@@ -14,18 +14,16 @@ object GearsReport extends ReportBuilder {
   // TODO: Add dustbin analysis (aggregated)
   // TODO: Add alignment stats per sample for the dustbin analysis
 
-  override def extFiles = super.extFiles ++ List(
-    "js/gears.js"
-  ).map(x => ExtFile("/nl/lumc/sasc/biopet/pipelines/gears/report/ext/" + x, x))
+  override def extFiles = super.extFiles ++ List("js/gears.js")
+    .map(x => ExtFile("/nl/lumc/sasc/biopet/pipelines/gears/report/ext/" + x, x))
 
   def indexPage = {
     ReportPage(
-      List() ++
-        Map(
-          "Versions" -> ReportPage(List(), List((
-            "Executables" -> ReportSection("/nl/lumc/sasc/biopet/core/report/executables.ssp"
-            ))), Map())
-        ),
+      List(
+        "Versions" -> ReportPage(List(), List((
+          "Executables" -> ReportSection("/nl/lumc/sasc/biopet/core/report/executables.ssp"
+          ))), Map())
+      ),
       List(
         "Gears intro" -> ReportSection("/nl/lumc/sasc/biopet/pipelines/gears/gearsFront.ssp"),
         "Sunburst analysis" -> ReportSection("/nl/lumc/sasc/biopet/pipelines/gears/gearsSunburst.ssp")
