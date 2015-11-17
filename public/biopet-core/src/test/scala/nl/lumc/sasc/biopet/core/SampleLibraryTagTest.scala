343ac6e1e9ee68f7f5dba98c0d817914a94855cf
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-11-17 10:27:30 +0100

    Added test to SampleLibraryTag

---
+++ public/biopet-core/src/test/scala/nl/lumc/sasc/biopet/core/SampleLibraryTagTest.scala
@@ -0,0 +1,41 @@
+package nl.lumc.sasc.biopet.core
+
+import nl.lumc.sasc.biopet.utils.config.{Config, Configurable}
+import org.scalatest.Matchers
+import org.scalatest.testng.TestNGSuite
+import org.testng.annotations.Test
+
+/**
+  * Created by pjvanthof on 17/11/15.
+  */
+class SampleLibraryTagTest extends TestNGSuite with Matchers {
+  @Test
+  def testDefault: Unit = {
+    val o = new SampleLibraryTag {
+      override def root: Configurable = null
+      override def globalConfig = new Config(Map())
+    }
+    o.sampleId shouldBe None
+    o.libId shouldBe None
+  }
+
+  @Test
+  def testInherit: Unit = {
+    val o1 = new SampleLibraryTag {
+      override def root: Configurable = null
+      override def globalConfig = new Config(Map())
+    }
+    o1.sampleId = Some("sampleName")
+    o1.libId = Some("libName")
+    o1.sampleId shouldBe Some("sampleName")
+    o1.libId shouldBe Some("libName")
+
+    val o2 = new SampleLibraryTag {
+      override def root: Configurable = o1
+      override def globalConfig = new Config(Map())
+    }
+    o2.sampleId shouldBe o1.sampleId
+    o2.libId shouldBe o1.libId
+
+  }
+}
