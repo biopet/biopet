60fc26c395d032063ef17a34c785c16dc915067a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2015-11-24 18:14:59 +0100

    Convert breakdancer to sub pipeline of shiva

--- public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/svcallers/Breakdancer.scala
+++ public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/svcallers/Breakdancer.scala
@@ -1,6 +1,6 @@
 package nl.lumc.sasc.biopet.pipelines.shiva.svcallers
 
-import nl.lumc.sasc.biopet.extensions.breakdancer.Breakdancer
+import nl.lumc.sasc.biopet.extensions.breakdancer.{BreakdancerVCF, BreakdancerCaller, BreakdancerConfig}
 import nl.lumc.sasc.biopet.utils.config.Configurable
 
 /** Script for sv caler Breakdancer */
@@ -8,11 +8,16 @@ class Breakdancer(val root: Configurable) extends SvCaller {
   def name = "breakdancer"
 
   def biopetScript() {
-    //TODO: move minipipeline of breakdancer to here
     for ((sample, bamFile) <- inputBams) {
-      val breakdancerDir = new File(outputDir, sample)
-      val breakdancer = Breakdancer(this, bamFile, breakdancerDir)
-      addAll(breakdancer.functions)
+      val breakdancerSampleDir = new File(outputDir, sample)
+
+      // read config and set all parameters for the pipeline
+      logger.debug("Starting Breakdancer configuration")
+
+      val bdcfg = BreakdancerConfig(this, bamFile, new File(breakdancerSampleDir, sample + ".breakdancer.cfg"))
+      val breakdancer = BreakdancerCaller(this, bdcfg.output, new File(breakdancerSampleDir, sample + ".breakdancer.tsv"))
+      val bdvcf = BreakdancerVCF(this, breakdancer.output, new File(breakdancerSampleDir, sample + ".breakdancer.vcf"))
+      add(bdcfg, breakdancer, bdvcf)
     }
   }
 }
