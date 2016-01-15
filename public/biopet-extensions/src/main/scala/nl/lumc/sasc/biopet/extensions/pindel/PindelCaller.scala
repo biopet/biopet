8867a510cab5f9e82e10b1cf1c0ca2c287e02b9c
Wai Yi Leung <w.y.leung@lumc.nl>
2016-01-15 16:54:26 +0100

    Fix output path for pindel (pindel outputs to certain dir with prefix_)

--- public/biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/pindel/PindelCaller.scala
+++ public/biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/pindel/PindelCaller.scala
@@ -124,7 +124,7 @@ class PindelCaller(val root: Configurable) extends BiopetCommandLineFunction wit
     }
 
     // modify `output_prefix` to include samplename
-    output_prefix = new File(output_prefix + File.separator,  "sample" )
+    output_prefix = new File(output_prefix + File.separator, "sample")
 
     // set the output file
     output_file = new File(output_prefix, "_D")
