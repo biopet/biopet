5171e18cf59e79d2ef5d297852be1c02e0b43025
Wai Yi Leung <w.y.leung@lumc.nl>
2016-01-19 17:46:10 +0100

    smallRNA pipeline

--- public/TinyCap/src/main/scala/nl/lumc/sasc/biopet/pipelines/tinycap/TinyCap.scala
+++ public/TinyCap/src/main/scala/nl/lumc/sasc/biopet/pipelines/tinycap/TinyCap.scala
@@ -1,26 +1,40 @@
 package nl.lumc.sasc.biopet.pipelines.tinycap
 
 import nl.lumc.sasc.biopet.core.{ PipelineCommand, Reference }
+import nl.lumc.sasc.biopet.extensions.HtseqCount
 import nl.lumc.sasc.biopet.pipelines.mapping.MultisampleMappingTrait
 import nl.lumc.sasc.biopet.utils.config.Configurable
 import org.broadinstitute.gatk.queue.QScript
 
 /**
  * Created by pjvan_thof on 12/29/15.
+ * Design based on work from Henk Buermans (e-Mir)
+ * Implementation by wyleung started 19/01/16
  */
 class TinyCap(val root: Configurable) extends QScript with MultisampleMappingTrait with Reference {
   qscript =>
   def this() = this(null)
 
+  var annotationGff: File = config("annotation_gff")
+
   override def defaults = Map(
     "merge_strategy" -> "preprocessmergesam",
-    "mapping" -> Map("alinger" -> "bowtie"),
+    "mapping" -> Map("aligner" -> "bowtie"),
     "bowtie" -> Map(
       "chunkmbs" -> 256,
       "seedmms" -> 3,
       "seedlen" -> 25,
       "k" -> 5,
-      "best" -> true)
+      "best" -> true),
+    "sickle" -> Map(
+      "lengthThreshold" -> 15
+    ),
+    "cutadapt" -> Map(
+      "minimum_length" -> 15,
+      "q" -> 30,
+      "default_clip_mode" -> "both",
+      "times" -> 2
+    )
   )
 
   override def makeSample(id: String) = new Sample(id)
@@ -29,12 +43,26 @@ class TinyCap(val root: Configurable) extends QScript with MultisampleMappingTra
     override def addJobs(): Unit = {
       super.addJobs()
 
-      bamFile // Merged bam file
-      //TODO: count job on small rna
+      // Do expression counting for miRNA and siRNA
+      val htseqCount = new HtseqCount(qscript)
+      htseqCount.inputAlignment = bamFile.get
+      htseqCount.inputAnnotation = annotationGff
+      htseqCount.format = Option("bam")
+      htseqCount.stranded = Option("yes")
+      htseqCount.output = createFile(".exprcount.tsv")
+      add(htseqCount)
     }
   }
 
   override def summaryFile = new File(outputDir, "tinycap.summary.json")
+
+  override def summaryFiles: Map[String, File] = super.summaryFiles ++ Map(
+    "annotationGff" -> annotationGff
+  )
+
+  override def addMultiSampleJobs = {
+    super.addMultiSampleJobs
+  }
 }
 
 object TinyCap extends PipelineCommand
