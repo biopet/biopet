c34c6160869fbc80fca8925fe1595168407909f1
Wai Yi Leung <w.y.leung@lumc.nl>
2016-03-09 15:41:05 +0100

    style

--- public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/ShivaSvCalling.scala
+++ public/shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/ShivaSvCalling.scala
@@ -18,7 +18,7 @@ package nl.lumc.sasc.biopet.pipelines.shiva
 import nl.lumc.sasc.biopet.core.summary.SummaryQScript
 import nl.lumc.sasc.biopet.core.{ PipelineCommand, Reference, SampleLibraryTag }
 import nl.lumc.sasc.biopet.extensions.Pysvtools
-import nl.lumc.sasc.biopet.pipelines.shiva.svcallers.{ Breakdancer, Clever, Delly, SvCaller, _ }
+import nl.lumc.sasc.biopet.pipelines.shiva.svcallers._
 import nl.lumc.sasc.biopet.utils.config.Configurable
 import nl.lumc.sasc.biopet.utils.{ BamUtils, Logging }
 import org.broadinstitute.gatk.queue.QScript
