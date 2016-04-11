6894dd4dbe8fb41653dfd1d5074f060380946828
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-04-11 09:45:48 +0200

    Adding test to GensToVcf

---
+++ public/biopet-tools/src/test/scala/nl/lumc/sasc/biopet/tools/GensToVcfTest.scala
@@ -0,0 +1,48 @@
+package nl.lumc.sasc.biopet.tools
+
+import java.io.File
+import java.nio.file.Paths
+
+import org.scalatest.Matchers
+import org.scalatest.testng.TestNGSuite
+import org.testng.annotations.Test
+
+/**
+  * Created by pjvan_thof on 4/11/16.
+  */
+class GensToVcfTest extends TestNGSuite with Matchers {
+  @Test
+  def testGensOnly: Unit = {
+    val output = File.createTempFile("test.", ".vcf.gz")
+    GensToVcf.main(Array(
+      "--inputGenotypes", GensToVcfTest.resourcePath("/test.gens"),
+      "--outputVcf", output.getAbsolutePath,
+      "--referenceFasta", GensToVcfTest.resourcePath("/fake_chrQ.fa"),
+      "--contig", "chrQ",
+      "--samplesFile", GensToVcfTest.resourcePath("/gens.samples")
+    ))
+  }
+
+  @Test
+  def testGensInfo: Unit = {
+    val output = File.createTempFile("test.", ".vcf.gz")
+    GensToVcf.main(Array(
+      "--inputGenotypes", GensToVcfTest.resourcePath("/test.gens"),
+      "--inputInfo", GensToVcfTest.resourcePath("/test.gens_info"),
+      "--outputVcf", output.getAbsolutePath,
+      "--referenceFasta", GensToVcfTest.resourcePath("/fake_chrQ.fa"),
+      "--contig", "chrQ",
+      "--samplesFile", GensToVcfTest.resourcePath("/gens.samples"),
+      "--sortInput"
+    ))
+  }
+
+}
+
+object GensToVcfTest {
+  private def resourcePath(p: String): String = {
+    Paths.get(getClass.getResource(p).toURI).toString
+  }
+
+
+}
