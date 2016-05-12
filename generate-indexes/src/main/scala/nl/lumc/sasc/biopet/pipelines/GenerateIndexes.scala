8ab8f962732930268104b7ad504603534edb4a55
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-05-12 08:31:03 +0200

    fix some code warnings

--- generate-indexes/src/main/scala/nl/lumc/sasc/biopet/pipelines/GenerateIndexes.scala
+++ generate-indexes/src/main/scala/nl/lumc/sasc/biopet/pipelines/GenerateIndexes.scala
@@ -94,7 +94,7 @@ class GenerateIndexes(val root: Configurable) extends QScript with BiopetQScript
           def commandLine = cmds.mkString(" && ")
         }
 
-        if (fastaUris.length > 1 || fastaFiles.filter(_.getName.endsWith(".gz")).nonEmpty) {
+        if (fastaUris.length > 1 || fastaFiles.exists(_.getName.endsWith(".gz"))) {
           fastaFiles.foreach { file =>
             if (file.getName.endsWith(".gz")) {
               val zcat = new Zcat(this)
@@ -159,14 +159,13 @@ class GenerateIndexes(val root: Configurable) extends QScript with BiopetQScript
 
           val regex = """.*\/(.*)_vep_(\d*)_(.*)\.tar\.gz""".r
           vepCacheUri.toString match {
-            case regex(species, version, assembly) if (version.forall(_.isDigit)) => {
+            case regex(species, version, assembly) if version.forall(_.isDigit) =>
               outputConfig ++= Map("varianteffectpredictor" -> Map(
                 "species" -> species,
                 "assembly" -> assembly,
                 "cache_version" -> version.toInt,
                 "cache" -> vepDir,
                 "fasta" -> createLinks(vepDir)))
-            }
             case _ => throw new IllegalArgumentException("Cache found but no version was found")
           }
         }
