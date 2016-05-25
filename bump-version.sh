55b1b7ddb22512d5db9f0c043f6701ce406be098
Sander Bollen <a.h.b.bollen@lumc.nl>
2016-05-25 16:40:19 +0200

    change ALL licenses across all files.

--- bump-version.sh
+++ bump-version.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #
-# Copyright (c) 2014 Leiden University Medical Center - Sequencing Analysis Support Core <sasc@lumc.nl>
-# @author Peter van 't Hof <p.j.van_t_hof@lumc.nl>
-# 
-# Script used to change version in pom files for Biopet project
-# 
+# Biopet is built on top of GATK Queue for building bioinformatic
+# pipelines. It is mainly intended to support LUMC SHARK cluster which is running
+# SGE. But other types of HPC that are supported by GATK Queue (such as PBS)
+# should also be able to execute Biopet tools and pipelines.
+#
+# Copyright 2014 Sequencing Analysis Support Core - Leiden University Medical Center
+#
+# Contact us at: sasc@lumc.nl
+#
+# A dual licensing mode is applied. The source code within this project is freely available for non-commercial use under an AGPL
+# license; For commercial users or users who do not want to follow the AGPL
+# license, please contact us to obtain a separate license.
+#
 
 DIR=`readlink -f \`dirname $0\``
 
