b55361cdcb8feff143d6bbc435e5be6943748031
Wai Yi Leung <w.y.leung@lumc.nl>
2016-06-24 14:35:40 +0200

    add test for WipeReads, check on instance in Gentrap

--- gentrap/src/test/scala/nl/lumc/sasc/biopet/pipelines/gentrap/GentrapTest.scala
+++ gentrap/src/test/scala/nl/lumc/sasc/biopet/pipelines/gentrap/GentrapTest.scala
@@ -14,20 +14,20 @@
  */
 package nl.lumc.sasc.biopet.pipelines.gentrap
 
-import java.io.{ File, FileOutputStream }
+import java.io.{File, FileOutputStream}
 
 import com.google.common.io.Files
-import nl.lumc.sasc.biopet.core.{ BiopetFifoPipe, BiopetPipe }
+import nl.lumc.sasc.biopet.core.{BiopetFifoPipe, BiopetPipe}
 import nl.lumc.sasc.biopet.extensions._
 import nl.lumc.sasc.biopet.extensions.gmap.Gsnap
 import nl.lumc.sasc.biopet.extensions.hisat.Hisat2
-import nl.lumc.sasc.biopet.extensions.tools.BaseCounter
+import nl.lumc.sasc.biopet.extensions.tools.{BaseCounter, WipeReads}
 import nl.lumc.sasc.biopet.utils.ConfigUtils
 import nl.lumc.sasc.biopet.utils.config.Config
 import org.broadinstitute.gatk.queue.QSettings
 import org.scalatest.Matchers
 import org.scalatest.testng.TestNGSuite
-import org.testng.annotations.{ DataProvider, Test }
+import org.testng.annotations.{DataProvider, Test}
 
 abstract class GentrapTestAbstract(val expressionMeasure: String, val aligner: Option[String]) extends TestNGSuite with Matchers {
 
@@ -144,6 +144,10 @@ abstract class GentrapTestAbstract(val expressionMeasure: String, val aligner: O
     if (expMeasures.contains("cufflinks_blind")) {
       assert(gentrap.functions.exists(_.isInstanceOf[Cufflinks]))
       assert(gentrap.functions.exists(_.isInstanceOf[Ln]))
+    }
+
+    if (gentrap.removeRibosomalReads) {
+      assert(gentrap.functions.exists(_.isInstanceOf[WipeReads]))
     }
 
     val classMap = Map(
