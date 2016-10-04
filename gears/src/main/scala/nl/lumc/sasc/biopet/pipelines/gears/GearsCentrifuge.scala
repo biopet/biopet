a0ee046e76ad548c273638b4b245777a58c922ab
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-10-04 08:00:08 +0200

    Remove default

--- gears/src/main/scala/nl/lumc/sasc/biopet/pipelines/gears/GearsCentrifuge.scala
+++ gears/src/main/scala/nl/lumc/sasc/biopet/pipelines/gears/GearsCentrifuge.scala
@@ -20,7 +20,6 @@ class GearsCentrifuge(val root: Configurable) extends QScript with SummaryQScrip
   var outputName: String = _
 
   override def fixedValues = Map("centrifugekreport" -> Map("only_unique" -> false))
-  override def defaults = Map("centrifugekreport" -> Map("show_zeros" -> true))
 
   def init(): Unit = {
     require(fastqR1 != null)
