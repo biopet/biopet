c1fb2254a3ef1a9b7e260bfd57df3956b5fb80a4
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-11-18 19:22:24 +0100

    Fixing BIOPET-353

--- biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/BiopetQScript.scala
+++ biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/BiopetQScript.scala
@@ -111,7 +111,8 @@ trait BiopetQScript extends Configurable with GatkLogging { qscript: QScript =>
 
     functions.filter(_.jobOutputFile == null).foreach(f => {
       try {
-        f.jobOutputFile = new File(f.firstOutput.getAbsoluteFile.getParent, "." + f.firstOutput.getName + "." + f.getClass.getSimpleName + ".out")
+        val className = if (f.getClass.isAnonymousClass) f.getClass.getSuperclass.getSimpleName else f.getClass.getSimpleName
+        f.jobOutputFile = new File(f.firstOutput.getAbsoluteFile.getParent, "." + f.firstOutput.getName + "." + className + ".out")
       } catch {
         case e: NullPointerException => logger.warn(s"Can't generate a jobOutputFile for $f")
       }
