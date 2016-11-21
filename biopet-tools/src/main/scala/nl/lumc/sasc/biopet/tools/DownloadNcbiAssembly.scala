35a30e140b358b1cc43e70ba5e382b6b4ea8b1ad
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-11-21 16:33:14 +0100

    Added a bit more docs to arg

--- biopet-tools/src/main/scala/nl/lumc/sasc/biopet/tools/DownloadNcbiAssembly.scala
+++ biopet-tools/src/main/scala/nl/lumc/sasc/biopet/tools/DownloadNcbiAssembly.scala
@@ -30,7 +30,13 @@ object DownloadNcbiAssembly extends ToolCommand {
     } text "where to write report from ncbi"
     opt[String]("nameHeader") unbounded () valueName "<string>" action { (x, c) =>
       c.copy(contigNameHeader = Some(x))
-    } text "What column to use from the NCBI report for the nam,e of the contigs"
+    } text
+      """
+        | What column to use from the NCBI report for the name of the contigs.
+        | All columns in the report can be used but this are the most common field to choose from:
+        | - 'Sequence-Name': Name of the contig within the assembly
+        | - 'UCSC-style-name': Name of the contig used by UCSC ( like hg19 )
+        | - 'RefSeq-Accn': Unique name of the contig at RefSeq (default for NCBI)""".stripMargin
     opt[(String, String)]("mustHaveOne") unbounded () valueName "<column_name=regex>" action { (x, c) =>
       c.copy(mustHaveOne = (x._1, x._2) :: c.mustHaveOne)
     } text "This can be used to filter based on the NCBI report, multiple conditions can be given, at least 1 should be true"
