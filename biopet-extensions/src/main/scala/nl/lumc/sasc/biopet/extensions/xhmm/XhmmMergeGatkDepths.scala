03af79a2ce076229e09201c6cc9cb39b82ad7adc
Sander Bollen <a.h.b.bollen@lumc.nl>
2016-11-23 13:44:43 +0100

    mergegatkdepths wrapper

---
+++ biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/xhmm/XhmmMergeGatkDepths.scala
@@ -0,0 +1,23 @@
+package nl.lumc.sasc.biopet.extensions.xhmm
+
+import java.io.File
+
+import nl.lumc.sasc.biopet.utils.config.Configurable
+import org.broadinstitute.gatk.utils.commandline.{Input, Output}
+
+/**
+  * Created by Sander Bollen on 23-11-16.
+  */
+class XhmmMergeGatkDepths(val root: Configurable) extends Xhmm {
+
+  @Input(doc = "List of input depths files")
+  var gatkDepthsFiles: List[File] = Nil
+
+  @Output(doc = "Merged output file in XHMM format")
+  var output: File = _
+
+  def cmdLine = {
+    executable + repeat("--GATKdepths", gatkDepthsFiles) + required("-o", output)
+  }
+
+}
