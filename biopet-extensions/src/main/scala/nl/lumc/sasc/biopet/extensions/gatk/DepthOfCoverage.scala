f70051f1843915cfec4f49c7625e43660a9e58d6
Sander Bollen <a.h.b.bollen@lumc.nl>
2016-11-29 17:06:29 +0100

    re-arrange auto-generated files from depth of coverage

--- biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/gatk/DepthOfCoverage.scala
+++ biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/gatk/DepthOfCoverage.scala
@@ -18,45 +18,31 @@ class DepthOfCoverage(val root: Configurable) extends CommandLineGATK {
   @Output(fullName = "out", shortName = "o", doc = "File name base to which coverage metric should be written")
   var out: File = _
 
-  @Output
-  lazy val summaryFile: File = {
-    new File(out + ".sample_summary")
-  }
+  private var _summaryFile: File = _
+  private var _statisticsFile: File = _
+  private var _intervalSummaryFile: File =  _
+  private var _intervalStatisticsFile: File = _
+  private var _geneSummaryFile: File = _
+  private var _geneStatisticsFile: File = _
+  private var _cumulativeCoverageCountsFile: File =  _
+  private var _cumulativeCoverageProportionsFile: File = _
 
   @Output
-  lazy val statisticsFile: File = {
-    new File(out + ".sample_statistics")
-  }
-
+  var summaryFile = _summaryFile
   @Output
-  lazy val intervalSummaryFile: File = {
-    new File(out + ".sample_interval_summary")
-  }
-
+  var statisticsFile = _statisticsFile
   @Output
-  lazy val intervalStatisticsFile: File = {
-    new File(out + ".sample_interval_statistics")
-  }
-
+  var intervalSummaryFile = _intervalSummaryFile
   @Output
-  lazy val geneSummaryFile: File = {
-    new File(out + ".sample_gene_summary")
-  }
-
+  var intervalStatisticsFile = _intervalStatisticsFile
   @Output
-  lazy val geneStatisticsFile: File = {
-    new File(out + ".sample_gene_statistics")
-  }
-
+  var geneSummaryFile = _geneSummaryFile
   @Output
-  lazy val cumulativeCoverageCountsFile: File = {
-    new File(out + ".sample_cumulative_coverage_counts")
-  }
-
+  var geneStatisticsFile = _geneStatisticsFile
   @Output
-  lazy val cumulativeCoverageProportionsFile: File = {
-    new File(out + ".sample_cumulative_coverage_proportions")
-  }
+  var culumativeCoverageCountsFile = _cumulativeCoverageCountsFile
+  @Output
+  var cumulativeCoverageProportionsFile = _cumulativeCoverageProportionsFile
 
   @Argument(required = false)
   var calculateCoveraOverGenes: Option[File] = None
@@ -120,6 +106,21 @@ class DepthOfCoverage(val root: Configurable) extends CommandLineGATK {
 
   @Argument(required = false)
   var printBinEndpointsAndExit: Boolean = false
+
+  override def beforeGraph() = {
+    super.beforeGraph()
+    if (out == null) {
+      throw new IllegalStateException("You must set the <out> variable")
+    }
+    _summaryFile = new File(out + ".sample_summary")
+    _statisticsFile = new File(out + ".sample_statistics")
+    _intervalSummaryFile =  new File(out + ".sample_interval_summary")
+    _intervalStatisticsFile = new File(out + ".sample_interval_statistics")
+    _geneSummaryFile = new File(out + ".sample_gene_summary")
+    _geneStatisticsFile = new File(out + ".sample_gene_statistics")
+    _cumulativeCoverageCountsFile =  new File(out + ".sample_cumulative_coverage_counts")
+    _cumulativeCoverageProportionsFile = new File(out + ".sample_cumulative_coverage_proportions")
+  }
 
   override def cmdLine = {
     super.cmdLine + required("--out", out) +
