2c35b3fae19dba1ff8fbf306e4a42d65943092ff
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-12-18 11:06:51 +0100

    Added sample config validations

--- biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/TemplateTool.scala
+++ biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/TemplateTool.scala
@@ -3,6 +3,7 @@ package nl.lumc.sasc.biopet.core
 import java.io.File
 
 import nl.lumc.sasc.biopet.utils.config.Config
+import nl.lumc.sasc.biopet.utils.summary.Summary
 import nl.lumc.sasc.biopet.utils.{ConfigUtils, Question, ToolCommand}
 
 /**
@@ -27,13 +28,16 @@ trait TemplateTool extends ToolCommand {
     val argsParser = new OptParser
     val cmdArgs: Args = argsParser.parse(args, Args()) getOrElse (throw new IllegalArgumentException)
 
-    val standard: Map[String, Any] = Map("output_dir" -> Question.string("Output directory", validation = List(isAbsolutePath, parentIsWritable)))
+    val standard: Map[String, Any] = Map("output_dir" -> Question.string("Output directory",
+      validation = List(isAbsolutePath, parentIsWritable)))
     val config = pipelineMap(standard, cmdArgs.expert)
 
     println(ConfigUtils.mapToYaml(config))
   }
 
   def pipelineMap(map: Map[String, Any], expert: Boolean): Map[String, Any]
+
+  val sampleConfigs: List[File] = Nil
 
 }
 
@@ -64,5 +68,35 @@ object TemplateTool {
       println(s"No premision to write $parent")
       false
     } else true
+  }
+
+  def askSampleConfigs(currentList: List[File] = Nil): List[File] = {
+    val configFile = new File(Question.string("Sample config file", validation = List(mustExist, isAbsolutePath)))
+    val configMap = new Summary(configFile)
+    println(s"${configMap.samples.size} samples found in config " +
+      s"with in total ${configMap.libraries.map(_._2.size).sum} libraries for '$configFile'")
+    if (Question.boolean("Is this correct?")) {
+      if (Question.boolean("Add more sample configs?")) askSampleConfigs(configFile :: currentList)
+      else {
+        val files = configFile :: currentList
+        if (files.size > 1) {
+          val configs = files.map(ConfigUtils.fileToConfigMap(_))
+          val sizes = configs.map(new Summary(_)).map(x => (x.samples.size, x.libraries.map(_._2.size).sum))
+          val samples = sizes.map(_._1).sum
+          val libs = sizes.map(_._2).sum
+          val mergedConfig = new Summary(configs.foldLeft(Map[String, Any]())((a,b) => ConfigUtils.mergeMaps(a, b)))
+          val mergesSamples = mergedConfig.samples.size
+          val mergesLibraries = mergedConfig.libraries.map(_._2.size).sum
+          if (mergesSamples != samples) println("WARNING: Overlapping samples detected")
+          if (mergesLibraries != libs) println("WARNING: Overlapping libraries detected")
+          println(s"$mergesSamples samples found in merged config with in total $mergesLibraries libraries")
+          if (Question.boolean("Is this correct?")) files
+          else {
+            println("Resetting sample configs")
+            askSampleConfigs()
+          }
+        } else files
+      }
+    } else askSampleConfigs(currentList)
   }
 }
--- shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/template/Shiva.scala
+++ shiva/src/main/scala/nl/lumc/sasc/biopet/pipelines/shiva/template/Shiva.scala
@@ -1,12 +1,20 @@
 package nl.lumc.sasc.biopet.pipelines.shiva.template
 
+import java.io.File
+
 import nl.lumc.sasc.biopet.core.{Reference, TemplateTool}
+import nl.lumc.sasc.biopet.utils.Question
 
 /**
   * Created by pjvanthof on 17/12/2016.
   */
 object Shiva extends TemplateTool {
+
+  override val sampleConfigs: List[File] = TemplateTool.askSampleConfigs()
+
   def pipelineMap(map: Map[String, Any], expert: Boolean): Map[String, Any] = {
-    map ++ Reference.askReference
+    map ++ Reference.askReference ++
+      Map("variantcallers" -> Question.list("Variantcallers",
+        posibleValues = List("haplotypercaller", "haplotypecaller_gvcf")))
   }
 }
