e6ed02971df811b26ef67231567117dcafc11f24
Sander Bollen <a.h.b.bollen@lumc.nl>
2017-01-18 16:50:02 +0100

    harmonize summary keys for flagstats

--- bammetrics/src/main/scala/nl/lumc/sasc/biopet/pipelines/bammetrics/BamMetrics.scala
+++ bammetrics/src/main/scala/nl/lumc/sasc/biopet/pipelines/bammetrics/BamMetrics.scala
@@ -158,14 +158,14 @@ class BamMetrics(val root: Configurable) extends QScript
         output = new File(targetDir, inputBam.getName.stripSuffix(".bam") + ".overlap.strict.sam"),
         minOverlap = config("strict_intersect_overlap", default = 1.0))
       val biopetFlagstatStrict = BiopetFlagstat(this, biStrict.output, targetDir)
-      addSummarizable(biopetFlagstatStrict, targetName + "_biopet_flagstat_strict")
+      addSummarizable(biopetFlagstatStrict, targetName + "_flagstats_strict")
       add(new BiopetFifoPipe(this, List(biStrict, biopetFlagstatStrict)))
 
       val biLoose = BedtoolsIntersect(this, inputBam, intervals.bed,
         output = new File(targetDir, inputBam.getName.stripSuffix(".bam") + ".overlap.loose.sam"),
         minOverlap = config("loose_intersect_overlap", default = 0.01))
       val biopetFlagstatLoose = BiopetFlagstat(this, biLoose.output, targetDir)
-      addSummarizable(biopetFlagstatLoose, targetName + "_biopet_flagstat_loose")
+      addSummarizable(biopetFlagstatLoose, targetName + "_flagstats_loose")
       add(new BiopetFifoPipe(this, List(biLoose, biopetFlagstatLoose)))
 
       val sortedBed = BamMetrics.sortedbedCache.getOrElse(intervals.bed, {
