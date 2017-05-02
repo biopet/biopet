dad1c45ad8584f2ce01b692feadd425797f1e744
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-05-02 10:11:48 +0200

    Adding exon and intron methods

--- biopet-tools/src/main/scala/nl/lumc/sasc/biopet/tools/refflatstats/RefflatStats.scala
+++ biopet-tools/src/main/scala/nl/lumc/sasc/biopet/tools/refflatstats/RefflatStats.scala
@@ -66,22 +66,41 @@ object RefflatStats extends ToolCommand {
 
   }
 
-  def generateGeneStats(gene: Gene, fastaFile: File) = Future {
+  def generateGeneStats(gene: Gene, fastaFile: File): Future[GeneStats] = Future {
     val referenceFile = new IndexedFastaSequenceFile(fastaFile)
     val contig = gene.getContig
     val start = List(gene.getStart, gene.getEnd).min
     val end = List(gene.getStart, gene.getEnd).max
     val gcCompleteGene = FastaUtils.getSequenceGc(referenceFile, contig, start, end)
 
-    val exons = (for (transcript <- gene) yield {
-      for (exon <- transcript.exons) yield {
-        val start = List(exon.start, exon.end).min
-        val end = List(exon.start, exon.end).max
-        BedRecord(contig, start, end)
-      }
-    }).flatten.toList.distinct.map(exon => exon -> exon.getGc(referenceFile)).toMap
+    val exons = geneToExonRegions(gene).distinct.map(exon => exon -> exon.getGc(referenceFile)).toMap
+    val introns = geneToIntronRegions(gene).distinct.map(exon => exon -> exon.getGc(referenceFile)).toMap
 
     referenceFile.close()
     s"${gene.getName} - $gcCompleteGene"
+  }
+
+  def geneToExonRegions(gene: Gene): List[BedRecord] = {
+    (for (transcript <- gene) yield {
+      for (exon <- transcript.exons) yield {
+        val start = List(exon.start, exon.end).min
+        val end = List(exon.start, exon.end).max
+        BedRecord(gene.getContig, start, end)
+      }
+    }).flatten.toList
+  }
+
+  def geneToIntronRegions(gene: Gene): List[BedRecord] = {
+    (for (transcript <- gene) yield {
+      if (transcript.exons.size > 1) {
+        for (i <- 0 until (transcript.exons.size - 2)) yield {
+          val intronStart = transcript.exons(i).end + 1
+          val intronEnd = transcript.exons(i + 1).start - 1
+          val start = List(intronStart, intronEnd).min
+          val end = List(intronStart, intronEnd).max
+          BedRecord(gene.getContig, start, end)
+        }
+      } else Nil
+    }).flatten.toList
   }
 }
