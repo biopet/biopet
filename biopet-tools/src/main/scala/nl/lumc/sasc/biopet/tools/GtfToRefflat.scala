93962a69c2372abdf3d2b5afccbc1afee9e75193
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-05-17 10:52:02 +0200

    Adding exons

---
+++ biopet-tools/src/main/scala/nl/lumc/sasc/biopet/tools/GtfToRefflat.scala
@@ -0,0 +1,140 @@
+package nl.lumc.sasc.biopet.tools
+
+import nl.lumc.sasc.biopet.utils.ToolCommand
+import java.io.{File, PrintWriter}
+
+import nl.lumc.sasc.biopet.utils.annotation.{Exon, Feature, Gene, Transcript}
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
+/**
+  * Created by pjvanthof on 16/05/2017.
+  */
+object GtfToRefflatextends extends ToolCommand {
+
+  case class Args(refFlat: File = null,
+                  gtfFile: File = null)
+    extends AbstractArgs
+
+  class OptParser extends AbstractOptParser {
+    opt[File]('r', "refFlat") required () valueName "<file>" action { (x, c) =>
+      c.copy(refFlat = x)
+    } text "Input refFlat file. Mandatory"
+    opt[File]('g', "gtfFile") required () valueName "<file>" action { (x, c) =>
+      c.copy(gtfFile = x)
+    } text "Output gtf file. Mandatory"
+  }
+
+  def main(args: Array[String]): Unit = {
+    val argsParser = new OptParser
+    val cmdArgs
+    : Args = argsParser.parse (args, Args () ) getOrElse (throw new IllegalArgumentException)
+
+    logger.info("Start")
+    gtfToRefflat(cmdArgs.gtfFile, cmdArgs.refFlat)
+    logger.info("Done")
+  }
+
+  def gtfToRefflat(gtfFile: File, refflatFile: File) = {
+    val reader = Source.fromFile(gtfFile)
+
+    val geneBuffer: mutable.Map[String, Gene] = mutable.Map()
+    val transcriptBuffer: mutable.Map[String, mutable.Map[String, Transcript]] = mutable.Map()
+    val exonBuffer: mutable.Map[(String, String), ListBuffer[Exon]] = mutable.Map()
+
+    val featureBuffer: mutable.Map[String, Int] = mutable.Map()
+
+    reader
+      .getLines()
+      .filter(!_.startsWith("#"))
+      .map(Feature.fromLine)
+      .foreach { case feature =>
+        featureBuffer += feature.feature -> (featureBuffer.getOrElse(feature.feature, 0) + 1)
+        val geneId = feature.attributes.get("gene_id") match {
+          case Some(id) => id
+          case _ => throw new IllegalArgumentException(s"Feature should have a gene_id, $feature")
+        }
+        def transcriptId = feature.attributes.get("transcript_id") match {
+          case Some(id) => id
+          case _ =>
+            throw new IllegalArgumentException(s"Feature should have a transcript_id, $feature")
+        }
+        feature.feature match {
+          case "gene" =>
+            if (geneBuffer.contains(geneId))
+              throw new IllegalArgumentException(s"Gene '$geneId' is found twice in $gtfFile")
+            geneBuffer += geneId -> Gene(geneId,
+              feature.contig,
+              feature.start,
+              feature.end,
+              feature.strand.getOrElse(true),
+              transcriptBuffer.remove(geneId).toList.flatMap(_.values).toList)
+          case "transcript" =>
+            val id = transcriptId
+            val exons = exonBuffer.remove((geneId, id)).getOrElse(Nil).toList
+            val transcript = Transcript(id,
+              feature.start,
+              feature.end,
+              feature.start,
+              feature.end,
+              exons)
+            if (geneBuffer.contains(geneId)) {
+              if (geneBuffer(geneId).transcripts.exists(_.name == id))
+                throw new IllegalArgumentException(s"Transcript '$id' is found twice in $gtfFile")
+              val gene = geneBuffer(geneId)
+              geneBuffer(geneId) = gene.copy(transcripts = gene.transcripts ::: transcript :: Nil)
+            } else { // Gene does not exist yet
+              if (!transcriptBuffer.contains(geneId)) transcriptBuffer += geneId -> mutable.Map()
+              if (transcriptBuffer(geneId).contains(id))
+                throw new IllegalArgumentException(s"Transcript '$id' is found twice in $gtfFile")
+              transcriptBuffer(geneId) += id -> transcript
+            }
+          case "exon" =>
+            val id = transcriptId
+            val exon = Exon(feature.start, feature.end)
+            (geneBuffer.get(geneId).flatMap(_.transcripts.find(_.name == id)), transcriptBuffer.get(geneId).flatMap(_.get(id))) match {
+              case (Some(transcript), _) =>
+                val gene = geneBuffer(geneId)
+                geneBuffer(geneId) = gene.copy(transcripts = transcript.copy(exons = exon :: transcript.exons) :: gene.transcripts.filter(_.name != transcript.name))
+              case (None, Some(transcript)) =>
+                transcriptBuffer(geneId)(id) = transcript.copy(exons = exon :: transcript.exons)
+              case _ =>
+                if (!exonBuffer.contains((geneId, id)))
+                  exonBuffer += (geneId, id) -> ListBuffer(exon)
+                else exonBuffer(geneId, id) += exon
+            }
+          case _ =>
+        }
+      }
+    reader.close()
+
+    featureBuffer.foreach { case (k,c) => logger.info(s"$k\t$c") }
+
+    val writer = new PrintWriter(refflatFile)
+
+    for {
+      gene <- geneBuffer.values
+      transcript <- gene.transcripts
+    } {
+      val exons = transcript.exons.sortBy(_.start)
+      val values: ListBuffer[String] = ListBuffer()
+      values += gene.name
+      values += transcript.name
+      values += gene.contig
+      values += (if (gene.strand) "+" else "-")
+      values += (transcript.transcriptionStart - 1).toString //TODO: check if this is correct
+      values += transcript.transcriptionEnd.toString
+      values += (transcript.codingStart - 1).toString //TODO: check if this is correct
+      values += transcript.codingEnd.toString //TODO: check if this is correct
+      values += transcript.exons.length.toString
+      values += exons.map(_.start - 1).mkString("",",",",") //TODO: check if this is correct
+      values += exons.map(_.end).mkString("",",",",")
+      writer.println(values.mkString("\t"))
+    }
+
+    writer.close()
+  }
+
+}
---
+++ biopet-utils/src/main/scala/nl/lumc/sasc/biopet/utils/annotation/Gene.scala
@@ -0,0 +1,10 @@
+package nl.lumc.sasc.biopet.utils.annotation
+
+/**
+  * Created by pjvanthof on 16/05/2017.
+  */
+case class Gene(name: String, contig: String, start: Int, end: Int, strand: Boolean, transcripts: List[Transcript])
+
+case class Transcript(name: String, transcriptionStart: Int, transcriptionEnd: Int, codingStart: Int, codingEnd: Int, exons: List[Exon])
+
+case class Exon(start: Int, end: Int)
