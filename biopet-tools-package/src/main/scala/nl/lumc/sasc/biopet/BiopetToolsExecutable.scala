8c2abd37a0d21b2b38ee93ad74c22c678b9284c0
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-06-17 22:17:48 +0200

    Adding to executable

--- biopet-tools-package/src/main/scala/nl/lumc/sasc/biopet/BiopetToolsExecutable.scala
+++ biopet-tools-package/src/main/scala/nl/lumc/sasc/biopet/BiopetToolsExecutable.scala
@@ -58,6 +58,7 @@ object BiopetToolsExecutable extends BiopetExecutable {
       nl.lumc.sasc.biopet.tools.VcfWithVcf,
       nl.lumc.sasc.biopet.tools.VepNormalizer,
       nl.lumc.sasc.biopet.tools.WipeReads,
+      nl.lumc.sasc.biopet.tools.MultiCoverage,
       nl.lumc.sasc.biopet.tools.NcbiReportToContigMap,
       nl.lumc.sasc.biopet.tools.DownloadNcbiAssembly
     )
