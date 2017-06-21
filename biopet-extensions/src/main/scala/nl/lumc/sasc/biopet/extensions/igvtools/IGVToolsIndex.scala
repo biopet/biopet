b3d175f91605f087cbc017fafc3907d7b126c0f3
akaljuvee <A.Kaljuvee@lumc.nl>
2017-06-21 22:08:27 +0200

    output file explicitly here, needed for the graph

--- biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/igvtools/IGVToolsIndex.scala
+++ biopet-extensions/src/main/scala/nl/lumc/sasc/biopet/extensions/igvtools/IGVToolsIndex.scala
@@ -2,15 +2,24 @@ package nl.lumc.sasc.biopet.extensions.igvtools
 
 import java.io.File
 
+import nl.lumc.sasc.biopet.utils.VcfUtils
 import nl.lumc.sasc.biopet.utils.config.Configurable
-import org.broadinstitute.gatk.utils.commandline.Input
+import org.broadinstitute.gatk.utils.commandline.{Input, Output}
 
 class IGVToolsIndex(val parent: Configurable) extends IGVTools {
 
   @Input(required = true)
   var input: File = _
 
+  @Output
+  var output: File = _
+
   override def cmdLine = super.cmdLine + required("index") + required(input)
+
+  override def beforeGraph(): Unit = {
+    super.beforeGraph()
+    output = VcfUtils.getVcfIndexFile(input)
+  }
 
 }
 
