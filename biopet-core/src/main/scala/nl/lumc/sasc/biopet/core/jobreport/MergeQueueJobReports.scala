52fd8f61cd5f6bb4c276b19fc47762b7e9ad555d
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-07-25 22:37:37 +0200

    Adding tool to merge jobreports

--- biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/jobreport/MergeQueueJobReports.scala
+++ biopet-core/src/main/scala/nl/lumc/sasc/biopet/core/jobreport/MergeQueueJobReports.scala
@@ -1,9 +1,9 @@
 package nl.lumc.sasc.biopet.core.jobreport
 
-import java.io.File
+import java.io.{File, PrintStream}
 
 import nl.lumc.sasc.biopet.utils.{AbstractOptParser, ToolCommand}
-import org.broadinstitute.gatk.utils.report.{GATKReport, GATKReportTable}
+import org.broadinstitute.gatk.utils.report.GATKReport
 
 import scala.collection.JavaConversions._
 
@@ -11,15 +11,15 @@ import scala.collection.JavaConversions._
   * Created by pjvanthof on 25/07/2017.
   */
 object MergeQueueJobReports extends ToolCommand {
-  case class Args(inputFiles: List[File] = Nil,
-                  outputfile: File = null)
+  case class Args(inputFiles: List[File] = Nil, outputfile: File = null)
 
   class OptParser extends AbstractOptParser[Args](commandName) {
-    opt[File]('I', "inputFile") unbounded() required () valueName "<file>" action { (x, c) =>
+    opt[File]('I', "inputFile") unbounded () required () valueName "<file>" action { (x, c) =>
       c.copy(inputFiles = x :: c.inputFiles)
     } text "Output directory of the pipeline"
-    opt[File]('o', "outputFile") unbounded() required () maxOccurs 1 valueName "<file>" action { (x, c) =>
-      c.copy(outputfile = x)
+    opt[File]('o', "outputFile") unbounded () required () maxOccurs 1 valueName "<file>" action {
+      (x, c) =>
+        c.copy(outputfile = x)
     } text "Output directory of this tool"
   }
 
@@ -28,19 +28,23 @@ object MergeQueueJobReports extends ToolCommand {
 
     val argsParser = new OptParser
     val cmdArgs
-    : Args = argsParser.parse(args, Args()) getOrElse (throw new IllegalArgumentException)
+      : Args = argsParser.parse(args, Args()) getOrElse (throw new IllegalArgumentException)
 
     val newReport = new GATKReport
     val reports = cmdArgs.inputFiles.map(new GATKReport(_))
-    val tableNames = reports.map(_.getTables.map(_.getTableName)).flatten.distinct
+    val tableNames = reports.flatMap(_.getTables.map(_.getTableName)).distinct
 
     for (name <- tableNames; report <- reports if report.hasTable(name)) {
       val table = report.getTable(name)
-      val newTable = table.concat(newReport.getTable(name))
-      //newReport.addTable(name, table.getTableDescription, newTable)
+      if (newReport.hasTable(name)) newReport.getTable(name).concat(table)
+      else newReport.addTable(table)
     }
 
-    println(tableNames)
+    val writer = new PrintStream(cmdArgs.outputfile)
+    newReport.print(writer)
+    writer.close()
+
+    logger.info("Done")
   }
 
 }
