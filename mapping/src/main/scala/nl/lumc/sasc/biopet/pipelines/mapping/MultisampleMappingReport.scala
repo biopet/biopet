baf0ba53f01998c3719a35ede63dfca5c3322f62
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-07-28 13:10:57 +0200

    centriguge typo

--- mapping/src/main/scala/nl/lumc/sasc/biopet/pipelines/mapping/MultisampleMappingReport.scala
+++ mapping/src/main/scala/nl/lumc/sasc/biopet/pipelines/mapping/MultisampleMappingReport.scala
@@ -110,7 +110,7 @@ trait MultisampleMappingReportTrait extends MultisampleReportBuilder {
                           Map())))
          else Nil) ++ (if (centrifugeExecuted)
                          List(
-                           "Centriguge analysis" -> Future.successful(ReportPage(
+                           "Centrifuge analysis" -> Future.successful(ReportPage(
                              List("Non-unique" -> Future.successful(
                                ReportPage(List(),
                                           List("All mappings" -> ReportSection(
@@ -203,7 +203,7 @@ trait MultisampleMappingReportTrait extends MultisampleReportBuilder {
            "Alignment" -> BammetricsReport.bamMetricsPage(summary, Some(sampleId), None)) ++
         (if (centrifugeExecuted)
            List(
-             "Centriguge analysis" -> Future.successful(ReportPage(
+             "Centrifuge analysis" -> Future.successful(ReportPage(
                List("Non-unique" -> Future.successful(ReportPage(
                  List(),
                  List("All mappings" -> ReportSection(
@@ -272,7 +272,7 @@ trait MultisampleMappingReportTrait extends MultisampleReportBuilder {
       ("Alignment" -> BammetricsReport.bamMetricsPage(summary, Some(sampleId), Some(libId))) ::
         (if (flexiprepExecuted) List("QC" -> FlexiprepReport.flexiprepPage) else Nil) :::
         (if (centrifugeExecuted)
-         List("Centriguge analysis" -> Future.successful(ReportPage(
+         List("Centrifuge analysis" -> Future.successful(ReportPage(
            List("Non-unique" -> Future.successful(
              ReportPage(List(),
                         List("All mappings" -> ReportSection(
