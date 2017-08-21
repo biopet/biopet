1ca4f8d498fcdd32b44d7283f93dd89c0fcffd87
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-08-21 16:16:49 +0200

    Style fixes

--- biopet-tools/src/test/scala/nl/lumc/sasc/biopet/tools/AddGenesAndTranscriptsToGtfTest.scala
+++ biopet-tools/src/test/scala/nl/lumc/sasc/biopet/tools/AddGenesAndTranscriptsToGtfTest.scala
@@ -15,16 +15,42 @@ class AddGenesAndTranscriptsToGtfTest extends TestNGSuite with Matchers {
   def testMain(): Unit = {
     val inputFile = File.createTempFile("test.", ".gtf")
     inputFile.deleteOnExit()
-    val gene1 = Feature("chrQ", "test", "gene", 11, 40, None, Some(true), None, Map("gene_id" -> "gene_1"))
-    val transcript1 = Feature("chrQ", "test", "transcript", 11, 40, None, Some(true), None, Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
-    val exon1 = Feature("chrQ", "test", "exon", 11, 20, None, Some(true), None, Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
-    val exon2 = Feature("chrQ", "test", "exon", 31, 40, None, Some(true), None, Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
+    val gene1 =
+      Feature("chrQ", "test", "gene", 11, 40, None, Some(true), None, Map("gene_id" -> "gene_1"))
+    val transcript1 = Feature("chrQ",
+                              "test",
+                              "transcript",
+                              11,
+                              40,
+                              None,
+                              Some(true),
+                              None,
+                              Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
+    val exon1 = Feature("chrQ",
+                        "test",
+                        "exon",
+                        11,
+                        20,
+                        None,
+                        Some(true),
+                        None,
+                        Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
+    val exon2 = Feature("chrQ",
+                        "test",
+                        "exon",
+                        31,
+                        40,
+                        None,
+                        Some(true),
+                        None,
+                        Map("gene_id" -> "gene_1", "transcript_id" -> "transcript_1_1"))
     IoUtils.writeLinesToFile(inputFile, List(exon1.asGtfLine, exon2.asGtfLine))
 
     val outputFile = File.createTempFile("test.", ".gtf")
     outputFile.deleteOnExit()
 
-    AddGenesAndTranscriptsToGtf.main(Array("-I", inputFile.getAbsolutePath, "-o", outputFile.getAbsolutePath))
+    AddGenesAndTranscriptsToGtf.main(
+      Array("-I", inputFile.getAbsolutePath, "-o", outputFile.getAbsolutePath))
 
     val reader = Source.fromFile(outputFile)
     val lines = reader.getLines().toList
